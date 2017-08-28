7a4a9891ca0cc66c7cc6fd8ea42d7a283efabe88
Beniamin Savu <benisavu@gmail.com>
2017-08-28 14:28:00 +0300

    aded test for restart merge

--- src/test/java/com/oxygenxml/sdksamples/workspace/git/service/GitAccessConflict.java
+++ src/test/java/com/oxygenxml/sdksamples/workspace/git/service/GitAccessConflict.java
@@ -13,37 +13,63 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.api.errors.TransportException;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.lib.RepositoryState;
 import org.junit.Test;
 
 import com.oxygenxml.git.options.OptionsManager;
+import com.oxygenxml.git.service.NoRepositorySelected;
 import com.oxygenxml.git.service.entities.FileStatus;
 import com.oxygenxml.git.service.entities.GitChangeType;
 
-public class GitAccessConflict extends GitAccessPullTest{
+public class GitAccessConflict extends GitAccessPullTest {
 
 	@Test
-	public void resolveUsingTheirs() throws RepositoryNotFoundException, FileNotFoundException, InvalidRemoteException, TransportException, IOException, GitAPIException{
+	public void testResolveUsingTheirs() throws RepositoryNotFoundException, FileNotFoundException,
+			InvalidRemoteException, TransportException, IOException, GitAPIException {
 		pushOneFileToRemote();
-		
+
 		gitAccess.setRepository(SECOND_LOCAL_TEST_REPOSITORY);
 		OptionsManager.getInstance().saveSelectedRepository(SECOND_LOCAL_TEST_REPOSITORY);
 		File file = new File(SECOND_LOCAL_TEST_REPOSITORY + "/test.txt");
 		file.createNewFile();
-		
+
 		PrintWriter out = new PrintWriter(LOCAL_TEST_REPOSITPRY + "/test.txt");
 		out.println("teeeeeest");
 		out.close();
-		
+
 		gitAccess.add(new FileStatus(GitChangeType.ADD, "test.txt"));
 		gitAccess.commit("conflict");
 		gitAccess.pull("", "");
 		gitAccess.updateWithRemoteFile("test.txt");
-		
+
 		String expected = "hellllo";
 		String actual = getFileContent();
 		assertEquals(expected, actual);
 	}
-	
+
+	@Test
+	public void testRestartMerge() throws RepositoryNotFoundException, FileNotFoundException, InvalidRemoteException, TransportException, IOException, GitAPIException, NoRepositorySelected {
+		pushOneFileToRemote();
+
+		gitAccess.setRepository(SECOND_LOCAL_TEST_REPOSITORY);
+		OptionsManager.getInstance().saveSelectedRepository(SECOND_LOCAL_TEST_REPOSITORY);
+		File file = new File(SECOND_LOCAL_TEST_REPOSITORY + "/test.txt");
+		file.createNewFile();
+
+		PrintWriter out = new PrintWriter(LOCAL_TEST_REPOSITPRY + "/test.txt");
+		out.println("teeeeeest");
+		out.close();
+
+		gitAccess.add(new FileStatus(GitChangeType.ADD, "test.txt"));
+		gitAccess.commit("conflict");
+		gitAccess.pull("", "");
+		gitAccess.addAll(gitAccess.getUnstagedFiles());
+		gitAccess.restartMerge();
+		RepositoryState actual = gitAccess.getRepository().getRepositoryState();
+		RepositoryState expected = RepositoryState.MERGING;
+		assertEquals(expected, actual);
+	}
+
 	private String getFileContent() throws FileNotFoundException, IOException {
 		FileReader fr = new FileReader(SECOND_LOCAL_TEST_REPOSITORY + "/test.txt");
 		BufferedReader br = new BufferedReader(fr);
