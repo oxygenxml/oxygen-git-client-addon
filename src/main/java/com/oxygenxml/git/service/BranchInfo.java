22dab4dc973885a2f4057a0caaf96df8d85b7426
Beniamin Savu <benisavu@gmail.com>
2017-09-25 17:07:39 +0300

    added some conmments

--- src/main/java/com/oxygenxml/git/service/BranchInfo.java
+++ src/main/java/com/oxygenxml/git/service/BranchInfo.java
@@ -1,9 +1,27 @@
 package com.oxygenxml.git.service;
 
+/**
+ * Stores information about the branch;
+ * 
+ * @author Beniamin Savu
+ *
+ */
 public class BranchInfo {
 
+	/**
+	 * The branch name
+	 */
 	private String branchName;
+
+	/**
+	 * Used to store the first 6 letters from the commit id. Only used on a
+	 * detached HEAD
+	 */
 	private String shortBranchName;
+
+	/**
+	 * <code>true</code> if the branch is detached <code>false</code> otherwise
+	 */
 	private boolean isDetached;
 
 	public BranchInfo() {
@@ -32,10 +50,10 @@ public class BranchInfo {
 		this.isDetached = isDetached;
 	}
 
-	public String getShortBranchName(){
+	public String getShortBranchName() {
 		return shortBranchName;
 	}
-	
+
 	public void setShortBranchName(String shortBranchName) {
 		this.shortBranchName = shortBranchName;
 	}
