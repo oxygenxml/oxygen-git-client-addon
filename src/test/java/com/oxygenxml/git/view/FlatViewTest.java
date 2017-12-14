e4cf96026453d283da715de1c08323bf198c27ef
Sorin Carbunaru <sorin_carbunaru@sync.ro>
2017-12-14 10:32:54 +0200

    Reorganized imports

--- src/test/java/com/oxygenxml/git/view/FlatViewTest.java
+++ src/test/java/com/oxygenxml/git/view/FlatViewTest.java
@@ -1,9 +1,6 @@
 package com.oxygenxml.git.view;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Iterator;
