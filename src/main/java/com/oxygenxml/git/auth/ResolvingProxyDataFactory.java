9ee8775be988ed2eb150820d8af82ed37268ccf9
AlexJitianu <alex_jitianu@sync.ro>
2019-07-03 10:18:47 +0300

    EXM-43655 Fixed NPE. Makes sure we don't pass a null list.

--- src/main/java/com/oxygenxml/git/auth/ResolvingProxyDataFactory.java
+++ src/main/java/com/oxygenxml/git/auth/ResolvingProxyDataFactory.java
@@ -2,8 +2,15 @@ package com.oxygenxml.git.auth;
 
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.net.ProxySelector;
+import java.net.SocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.eclipse.jgit.transport.sshd.DefaultProxyDataFactory;
 import org.eclipse.jgit.transport.sshd.ProxyData;
 
@@ -13,13 +20,17 @@ import org.eclipse.jgit.transport.sshd.ProxyData;
  * An attempt will be made to resolve the host name into an InetAddress. 
  */
 public class ResolvingProxyDataFactory extends DefaultProxyDataFactory {
+  /**
+   * Logger for logging.
+   */
+  private static final Logger logger = Logger.getLogger(ResolvingProxyDataFactory.class);
 
   /**
    * org.eclipse.jgit.transport.sshd.ProxyDataFactory.get(InetSocketAddress)
    */
   @Override
   public ProxyData get(InetSocketAddress remoteAddress) {
-    ProxyData proxyData = super.get(remoteAddress);
+    ProxyData proxyData = getInternal(remoteAddress);
     return newData(proxyData);
   }
 
@@ -63,5 +74,72 @@ public class ResolvingProxyDataFactory extends DefaultProxyDataFactory {
       }
     }
   }
+  
+  /**
+   * Just a copy of super.get()
+   * 
+   * @param remoteAddress
+   * @return
+   */
+  public ProxyData getInternal(InetSocketAddress remoteAddress) {
+    try {
+      
+      if (logger.isDebugEnabled()) {
+        logger.debug("Use proxy selector " + ProxySelector.getDefault());
+      }
+      
+      List<Proxy> proxies = ProxySelector.getDefault()
+          .select(new URI(Proxy.Type.SOCKS.name(),
+              "//" + remoteAddress.getHostString(), null)); //$NON-NLS-1$
+      
+      if (logger.isDebugEnabled()) {
+        logger.debug("Got SOCKS proxies " + proxies);
+      }
+      
+      if (proxies == null) {
+        proxies = new ArrayList<>();
+      }
+      
+      ProxyData data = getData(proxies, Proxy.Type.SOCKS);
+      if (data == null) {
+        proxies = ProxySelector.getDefault()
+            .select(new URI(Proxy.Type.HTTP.name(),
+                "//" + remoteAddress.getHostString(), //$NON-NLS-1$
+                null));
+        
+        if (logger.isDebugEnabled()) {
+          logger.debug("Got HTTP " + proxies);
+        }
+        
+        if (proxies == null) {
+          proxies = new ArrayList<>();
+        }
+        
+        data = getData(proxies, Proxy.Type.HTTP);
+      }
+      return data;
+    } catch (URISyntaxException e) {
+      return null;
+    }
+  }
 
+  private ProxyData getData(List<Proxy> proxies, Proxy.Type type) {
+    Proxy proxy = proxies.stream().filter(p -> type == p.type()).findFirst()
+        .orElse(null);
+    if (proxy == null) {
+      return null;
+    }
+    SocketAddress address = proxy.address();
+    if (!(address instanceof InetSocketAddress)) {
+      return null;
+    }
+    switch (type) {
+    case HTTP:
+      return new ProxyData(proxy);
+    case SOCKS:
+      return new ProxyData(proxy);
+    default:
+      return null;
+    }
+  }
 }
