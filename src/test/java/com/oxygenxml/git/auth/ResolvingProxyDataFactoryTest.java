45d3311958d3ba3bc06c722b774b255f5eeaa893
Sorin Carbunaru <sorin_carbunaru@sync.ro>
2019-07-08 17:22:28 +0300

    EXM-43640: test case

---
+++ src/test/java/com/oxygenxml/git/auth/ResolvingProxyDataFactoryTest.java
@@ -0,0 +1,32 @@
+package com.oxygenxml.git.auth;
+
+import java.io.File;
+
+import org.eclipse.jgit.transport.sshd.DefaultProxyDataFactory;
+
+import junit.framework.TestCase;
+
+public class ResolvingProxyDataFactoryTest extends TestCase {
+
+  /**
+   * <p><b>Description:</b> When upgrading the Apache MINA SSHD version,
+   * check if the "socks" URI schema has been changed to "socket".
+   * It should be changed in version 5.5. Also check any other changes,
+   * and keep our patches, if needed.</p>
+   * <p><b>Bug ID:</b> EXM-43640</p>
+   *
+   * @author sorin_carbunaru
+   *
+   * @throws Exception
+   */
+  public void testApacheMINAVersionAndPatches() throws Exception {
+    String name = new File(DefaultProxyDataFactory.class.getProtectionDomain()
+        .getCodeSource().getLocation().toURI()).getName();
+    assertEquals("The Apache MINA SSHD library has been updated."
+        + " Check " + ResolvingProxyDataFactory.class.getName() + " for "
+        + "updating the patches, if needed.",
+        "org.eclipse.jgit.ssh.apache-5.4.0.201906121030-r.jar",
+        name);
+  }
+  
+}
