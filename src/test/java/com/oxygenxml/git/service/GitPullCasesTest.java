51404327c15a40b97ab09fdf3bf4d38e0c3558d3
Sorin Carbunaru <sorin_carbunaru@sync.ro>
2019-10-21 10:16:25 +0300

    Logging (temporary)

--- src/test/java/com/oxygenxml/git/service/GitPullCasesTest.java
+++ src/test/java/com/oxygenxml/git/service/GitPullCasesTest.java
@@ -455,11 +455,23 @@ public class GitPullCasesTest extends GitTestBase {
     String repoURI = new File("target/test-resources/GitPullCasesTest/testPullRebase-remote/.git/")
         .toURI()
         .toString();
+    
+    System.err.println();
+    System.err.println("================================");
+    System.err.println("repoURI.startsWith(\"file:/\"): " + repoURI.startsWith("file:/"));
+    System.err.println("!repoURI.startsWith(\"file:///\"): " + !repoURI.startsWith("file:///"));
+    System.err.println("System.getProperty(\"os.name\").toUpperCase(): " + System.getProperty("os.name").toUpperCase());
+    System.err.println("isWin: " + System.getProperty("os.name").toUpperCase().startsWith("WIN"));
+    System.err.println("================================");
+    System.err.println();
+    
     if (repoURI.startsWith("file:/")
         && !repoURI.startsWith("file:///")
         // Only on Windows
         && System.getProperty("os.name").toUpperCase().startsWith("WIN")) {
+      System.err.println("OLD: " + repoURI);
       repoURI = "file:///" + repoURI.substring("file:/".length());
+      System.err.println("NEW: " + repoURI);
     }
     assertEquals(
         "Commits from last to first:\n" + 
