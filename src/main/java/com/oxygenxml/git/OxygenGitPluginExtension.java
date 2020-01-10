a29f5d57e7807f9bacc896616d629716008efd72
Sorin Carbunaru <sorin_carbunaru@sync.ro>
2020-01-10 14:26:49 +0200

    EXM-44470: create views' panels only once

--- src/main/java/com/oxygenxml/git/OxygenGitPluginExtension.java
+++ src/main/java/com/oxygenxml/git/OxygenGitPluginExtension.java
@@ -34,7 +34,6 @@ import com.oxygenxml.git.view.historycomponents.HistoryPanel;
 
 import ro.sync.exml.plugin.workspace.WorkspaceAccessPluginExtension;
 import ro.sync.exml.workspace.api.standalone.StandalonePluginWorkspace;
-import ro.sync.exml.workspace.api.standalone.ViewComponentCustomizer;
 import ro.sync.exml.workspace.api.standalone.ViewInfo;
 
 /**
@@ -53,6 +52,7 @@ public class OxygenGitPluginExtension implements WorkspaceAccessPluginExtension,
 	 * ID of the Git staging view. Defined in plugin.xml.
 	 */
 	static final String GIT_STAGING_VIEW = "GitStagingView";
+	
 	/**
 	 * ID of the Git History view.
 	 */
@@ -62,11 +62,40 @@ public class OxygenGitPluginExtension implements WorkspaceAccessPluginExtension,
 	 * Refresh support.
 	 */
 	final PanelRefresh gitRefreshSupport = new PanelRefresh();
+	
+	/**
+	 * Window listener used to call the refresh command when the Oxygen window is activated
+	 */
+  private WindowAdapter panelRefreshWindowListener = new WindowAdapter() {
+
+    private boolean refresh = false;
+
+    @Override
+    public void windowActivated(WindowEvent e) {
+      super.windowActivated(e);
+      if (refresh && stagingPanel != null && stagingPanel.isShowing()) {
+        gitRefreshSupport.call();
+      }
+      refresh = false;
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent e) {
+      super.windowDeactivated(e);
+      SwingUtilities.invokeLater(() -> {
+        Object focusedWindow = KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusedWindow();
+        if (focusedWindow == null) {
+          refresh = true;
+        }
+      });
+    }
+  };
 
 	/**
 	 * Staging panel.
 	 */
   private StagingPanel stagingPanel;
+  
   /**
    * Plugin workspace access.
    */
@@ -76,13 +105,13 @@ public class OxygenGitPluginExtension implements WorkspaceAccessPluginExtension,
    * History view.
    */
   private HistoryPanel historyView;
-	
+  
 	/**
-	 * @see ro.sync.exml.plugin.workspace.WorkspaceAccessPluginExtension#applicationStarted(ro.sync.exml.workspace.api.standalone.StandalonePluginWorkspace)
+	 * @see WorkspaceAccessPluginExtension#applicationStarted(StandalonePluginWorkspace)
 	 */
 	@Override
-	public void applicationStarted(final StandalonePluginWorkspace pluginWorkspaceAccess) {
-	  this.pluginWorkspaceAccess = pluginWorkspaceAccess;
+	public void applicationStarted(final StandalonePluginWorkspace pluginWS) {
+	  pluginWorkspaceAccess = pluginWS;
 		try {
 		  // Uncomment this to start with fresh options. For testing purposes
 //			PluginWorkspaceProvider.getPluginWorkspace().getOptionsStorage().setOption("GIT_PLUGIN_OPTIONS", null); NOSONAR
@@ -94,74 +123,21 @@ public class OxygenGitPluginExtension implements WorkspaceAccessPluginExtension,
 		  
 		  AuthenticationInterceptor.install();
 
-			GitController stageController = new GitController();
+			GitController gitCtrl = new GitController();
 			
 			ProjectViewManager.addPopUpMenuCustomizer(
 			    pluginWorkspaceAccess,
-			    new GitMenuActionsProvider(pluginWorkspaceAccess, stageController));
+			    new GitMenuActionsProvider(pluginWorkspaceAccess, gitCtrl));
 
 			pluginWorkspaceAccess.addViewComponentCustomizer(
-			    new ViewComponentCustomizer() {// NOSONAR
-				/**
-				 * @see ro.sync.exml.workspace.api.standalone.ViewComponentCustomizer#customizeView(ro.sync.exml.workspace.api.standalone.ViewInfo)
-				 */
-			  @Override
-			  public void customizeView(ViewInfo viewInfo) {
-			    // The constant's value is defined in plugin.xml
-			    if (GIT_STAGING_VIEW.equals(viewInfo.getViewID())) {
-			      stagingPanel = new StagingPanel(gitRefreshSupport, stageController, OxygenGitPluginExtension.this);
-			      gitRefreshSupport.setPanel(stagingPanel);
-			      viewInfo.setComponent(stagingPanel);
-			      
-			      GitAccess.getInstance().addGitListener(new GitEventAdapter() {
-			        @Override
-			        public void repositoryIsAboutToOpen(File repo) {
-			          SwingUtilities.invokeLater(() -> viewInfo.getComponent().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR)));
-			        }
-			        @Override
-              public void repositoryChanged() {
-			          SwingUtilities.invokeLater(() -> viewInfo.getComponent().setCursor(Cursor.getDefaultCursor()));
-			        }
-			        @Override
-              public void repositoryOpeningFailed(File repo, Throwable ex) {
-			          SwingUtilities.invokeLater(() -> viewInfo.getComponent().setCursor(Cursor.getDefaultCursor()));
-			        }
-			       
-			        private Timer cursorTimer = new Timer(
-			            1000,
-			            e -> SwingUtilities.invokeLater(() -> viewInfo.getComponent().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR))));
-			        @Override
-			        public void stateChanged(com.oxygenxml.git.view.event.GitEvent changeEvent) {
-                GitCommand cmd = changeEvent.getGitCommand();
-                GitCommandState cmdState = changeEvent.getGitComandState();
-                if (cmdState == GitCommandState.STARTED) {
-                  cursorTimer.restart();
-                } else if (cmdState == GitCommandState.SUCCESSFULLY_ENDED
-                    || cmdState == GitCommandState.FAILED) {
-                  cursorTimer.stop();
-                  SwingUtilities.invokeLater(() -> viewInfo.getComponent().setCursor(Cursor.getDefaultCursor()));
-                
-                  if (cmd == GitCommand.CONTINUE_REBASE) {
-                    gitRefreshSupport.call();
-                  }
-                }
-			        }
-			      });
-			      
-			      // Start the thread that populates the view.
-			      gitRefreshSupport.call();
-			      
-			      viewInfo.setIcon(Icons.getIcon(Icons.GIT_ICON));
-			      viewInfo.setTitle(Translator.getInstance().getTranslation(Tags.GIT_STAGING));
-					} else if (GIT_HISTORY_VIEW.equals(viewInfo.getViewID())) {
-					  historyView = new HistoryPanel(stageController);
-            viewInfo.setComponent(historyView);
-            
-            viewInfo.setIcon(Icons.getIcon(Icons.GIT_HISTORY));
-            viewInfo.setTitle(Translator.getInstance().getTranslation(Tags.GIT_HISTORY));
-					}
-				}
-			});
+			    viewInfo -> {
+            // The constants' values are defined in plugin.xml
+            if (GIT_STAGING_VIEW.equals(viewInfo.getViewID())) {
+              customizeGitStagingView(gitCtrl, viewInfo);
+          	} else if (GIT_HISTORY_VIEW.equals(viewInfo.getViewID())) {
+          	  customizeHistoryView(gitCtrl, viewInfo);
+          	}
+          });
 
 			final JFrame parentFrame = (JFrame) pluginWorkspaceAccess.getParentFrame();
 			
@@ -177,55 +153,12 @@ public class OxygenGitPluginExtension implements WorkspaceAccessPluginExtension,
 						pluginWorkspaceAccess.getOptionsStorage().setOption(key, "true");
 					}
 				}
-
 			});
 
 			// Call the refresh command when the Oxygen window is activated
-			parentFrame.addWindowListener(new WindowAdapter() {
-
-			  /**
-			   * <code>true</code> to refresh.
-			   */
-				private boolean refresh = false;
-
-				@Override
-				public void windowActivated(WindowEvent e) {
-					super.windowActivated(e);
-					if (refresh && stagingPanel != null && stagingPanel.isShowing()) {
-						gitRefreshSupport.call();
-					}
-					refresh = false;
-				}
-
-				@Override
-				public void windowDeactivated(WindowEvent e) {
-					super.windowDeactivated(e);
-					SwingUtilities.invokeLater(
-					    new Runnable() { // NOSONAR
-					  @Override
-						public void run() {
-							Object focusedWindow = KeyboardFocusManager.getCurrentKeyboardFocusManager().getFocusedWindow();
-							if (focusedWindow == null) {
-								refresh = true;
-							}
-						}
-					});
-				}
-			});
+      parentFrame.addWindowListener(panelRefreshWindowListener);
 			
-			// Redirect logging to the Log4J instance.
-			com.jcraft.jsch.JSch.setLogger(new com.jcraft.jsch.Logger() {
-			  @Override
-        public void log(int level, String message) {
-          if (logger.isDebugEnabled()) {
-            logger.debug(message);
-          }
-        }
-			  @Override
-        public boolean isEnabled(int level) {
-          return logger.isDebugEnabled();
-        }
-      });
+			installLog4JLogger();
 			
 		} catch (Throwable t) { // NOSONAR
 			// Catch Throwable - Runtime exceptions shouldn't affect Oxygen.
@@ -233,6 +166,97 @@ public class OxygenGitPluginExtension implements WorkspaceAccessPluginExtension,
 			logger.fatal(t, t);
 		}
 	}
+	
+	/**
+	 * Customize the Git Staging view.
+	 * 
+	 * @param gitCtrl  Git controller.
+	 * @param viewInfo View information.
+	 */
+	private void customizeGitStagingView(GitController gitCtrl, ViewInfo viewInfo) {
+    boolean shouldRecreateStagingPanel = stagingPanel == null;
+    if (shouldRecreateStagingPanel) {
+      stagingPanel = new StagingPanel(gitRefreshSupport, gitCtrl, OxygenGitPluginExtension.this);
+      gitRefreshSupport.setPanel(stagingPanel);
+    }
+    viewInfo.setComponent(stagingPanel);
+    
+    GitAccess.getInstance().addGitListener(new GitEventAdapter() {
+      @Override
+      public void repositoryIsAboutToOpen(File repo) {
+        SwingUtilities.invokeLater(() -> viewInfo.getComponent().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR)));
+      }
+      @Override
+      public void repositoryChanged() {
+        SwingUtilities.invokeLater(() -> viewInfo.getComponent().setCursor(Cursor.getDefaultCursor()));
+      }
+      @Override
+      public void repositoryOpeningFailed(File repo, Throwable ex) {
+        SwingUtilities.invokeLater(() -> viewInfo.getComponent().setCursor(Cursor.getDefaultCursor()));
+      }
+     
+      private Timer cursorTimer = new Timer(
+          1000,
+          e -> SwingUtilities.invokeLater(() -> viewInfo.getComponent().setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR))));
+      @Override
+      public void stateChanged(com.oxygenxml.git.view.event.GitEvent changeEvent) {
+        GitCommand cmd = changeEvent.getGitCommand();
+        GitCommandState cmdState = changeEvent.getGitComandState();
+        if (cmdState == GitCommandState.STARTED) {
+          cursorTimer.restart();
+        } else if (cmdState == GitCommandState.SUCCESSFULLY_ENDED || cmdState == GitCommandState.FAILED) {
+          cursorTimer.stop();
+          SwingUtilities.invokeLater(() -> viewInfo.getComponent().setCursor(Cursor.getDefaultCursor()));
+        
+          if (cmd == GitCommand.CONTINUE_REBASE) {
+            gitRefreshSupport.call();
+          }
+        }
+      }
+    });
+    
+    if (shouldRecreateStagingPanel) {
+      // Start the thread that populates the view.
+      gitRefreshSupport.call();
+    }
+    
+    viewInfo.setIcon(Icons.getIcon(Icons.GIT_ICON));
+    viewInfo.setTitle(Translator.getInstance().getTranslation(Tags.GIT_STAGING));
+  }
+	
+	/**
+	 * Customize the history view.
+	 * 
+	 * @param gitCtrl   Git controller.
+	 * @param viewInfo  View information.
+	 */
+  private void customizeHistoryView(GitController gitCtrl, ViewInfo viewInfo) {
+    if (historyView == null) {
+      historyView = new HistoryPanel(gitCtrl);
+    }
+    viewInfo.setComponent(historyView);
+    
+    viewInfo.setIcon(Icons.getIcon(Icons.GIT_HISTORY));
+    viewInfo.setTitle(Translator.getInstance().getTranslation(Tags.GIT_HISTORY));
+  }
+
+	/**
+	 * Redirect logging to the Log4J instance.
+	 */
+  private void installLog4JLogger() {
+    com.jcraft.jsch.JSch.setLogger(new com.jcraft.jsch.Logger() {
+      @Override
+      public void log(int level, String message) {
+        if (logger.isDebugEnabled()) {
+          logger.debug(message);
+        }
+      }
+      @Override
+      public boolean isEnabled(int level) {
+        return logger.isDebugEnabled();
+      }
+    });
+  }
 
 	/**
 	 * @see ro.sync.exml.plugin.workspace.WorkspaceAccessPluginExtension#applicationClosing()
