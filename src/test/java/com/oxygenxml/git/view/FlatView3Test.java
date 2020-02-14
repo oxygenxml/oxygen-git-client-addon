c091d65e27febd56dfb6f32aff719da448dbdfbd
AlexJitianu <jitianualex83@gmail.com>
2020-02-14 16:23:39 +0200

    Break tests.

---
+++ src/test/java/com/oxygenxml/git/view/FlatView3Test.java
@@ -0,0 +1,95 @@
+package com.oxygenxml.git.view;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.eclipse.jgit.lib.Repository;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.oxygenxml.git.service.entities.FileStatus;
+import com.oxygenxml.git.service.entities.GitChangeType;
+import com.oxygenxml.git.view.ChangesPanel.ResourcesViewMode;
+
+/**
+* Test cases related to the actions performed
+* on the staged/unstaged resources seen in the flat view.
+*/
+public class FlatView3Test extends FlatViewTestBase {
+  
+  @Before
+  public void setUp() throws Exception {
+    super.setUp();
+    
+    stagingPanel.getUnstagedChangesPanel().setResourcesViewMode(ResourcesViewMode.FLAT_VIEW);
+    stagingPanel.getStagedChangesPanel().setResourcesViewMode(ResourcesViewMode.FLAT_VIEW);
+  }
+  /**
+   * Discard changes. THose files must not appear in either stage/un-stage area.
+   * 
+   * @throws Exception If it fails.
+   */
+  @Test
+  public void testDiscard() throws Exception {
+    /**
+     * Local repository location.
+     */
+    String localTestRepository = "target/test-resources/testDiscard_NewFile_local";
+    
+    /**
+     * Remote repository location.
+     */
+    String remoteTestRepository = "target/test-resources/testDiscard_NewFile_remote";
+    
+    
+    new File(localTestRepository).mkdirs();
+    File file = new File(localTestRepository + "/test.txt");
+    file.createNewFile();
+    setFileContent(file, "remote");
+    
+    // Create repositories
+    Repository remoteRepo = createRepository(remoteTestRepository);
+    Repository localRepo = createRepository(localTestRepository);
+    // Bind the local repository to the remote one.
+    bindLocalToRemote(localRepo , remoteRepo);
+    
+    // Add it to the index.
+    add(new FileStatus(GitChangeType.ADD, "test.txt"));
+    assertTableModels("", "ADD, test.txt");
+    
+    gitAccess.commit("First version.");
+    
+    assertTableModels("", "");
+    
+    // Change the file.
+    setFileContent(file, "index content");
+    
+    assertTableModels("MODIFIED, test.txt", "");
+    
+    add(new FileStatus(GitChangeType.MODIFIED, "test.txt"));
+    
+    assertTableModels("", "CHANGED, test.txt");
+    
+    // Change the file.
+    setFileContent(file, "modified content");
+    
+    // The file is present in  both areas.
+    assertTableModels(
+        "MODIFIED, test.txt", 
+        "CHANGED, test.txt");
+    
+    // Discard.
+    DiscardAction discardAction = new DiscardAction(
+        Arrays.asList(new FileStatus(GitChangeType.MODIFIED, "test.txt")),
+        stagingPanel.getStageController());
+    
+    discardAction.actionPerformed(null);
+    
+    waitForScheduler();
+    
+    assertTableModels(
+        "", 
+        "");    
+  }
+
+}
