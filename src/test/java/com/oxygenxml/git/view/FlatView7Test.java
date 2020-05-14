9d9c499dea569cffbe371bbac960d2cda912d44b
Sorin Carbunaru <sorin_carbunaru@sync.ro>
2020-05-14 17:00:31 +0300

    sleep a bit

--- src/test/java/com/oxygenxml/git/view/FlatView7Test.java
+++ src/test/java/com/oxygenxml/git/view/FlatView7Test.java
@@ -116,6 +116,7 @@ public class FlatView7Test extends FlatViewTestBase {
     SwingUtilities.invokeLater(() -> commitPanel.getCommitButton().doClick());
     waitForScheluerBetter();
     flushAWT();
+    sleep(300);
     assertEquals(1, GitAccess.getInstance().getPushesAhead());
     assertFalse(amendBtn.isSelected());
     assertEquals("", commitPanel.getCommitMessageArea().getText());
