d2ff5d01bfb365cb097a8f67e5ba2021d0cb11a0
AlexJitianu <jitianualex83@gmail.com>
2020-07-30 17:40:15 +0300

    EXM-45229 #98 Changed a bit the messages. Added a bit of space. Use JRadioButton.

--- src/main/java/com/oxygenxml/git/OxygenGitOptionPagePluginExtension.java
+++ src/main/java/com/oxygenxml/git/OxygenGitOptionPagePluginExtension.java
@@ -1,24 +1,24 @@
 package com.oxygenxml.git;
 
-import javax.swing.JComponent;
-
-import ro.sync.exml.plugin.option.OptionPagePluginExtension;
-import ro.sync.exml.workspace.api.PluginWorkspace;
-
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 import javax.swing.ButtonGroup;
-import javax.swing.JCheckBox;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 import com.oxygenxml.git.options.OptionsManager;
 import com.oxygenxml.git.translator.Tags;
 import com.oxygenxml.git.translator.Translator;
 
+import ro.sync.exml.plugin.option.OptionPagePluginExtension;
+import ro.sync.exml.workspace.api.PluginWorkspace;
+
 /**
- * Plugin option page extension Repository Watcher Workspace Access Plugin Extension.
+ * Plugin option page extension.
  */
 public class OxygenGitOptionPagePluginExtension extends OptionPagePluginExtension{
  
@@ -33,39 +33,34 @@ public class OxygenGitOptionPagePluginExtension extends OptionPagePluginExtensio
   public static final String WARN_UPSTREAM_ON_CHANGE = "onChange";
   
   /**
-   * Value for never notifying when there are new commits upstream.
+   * Value to skip any checks on the upstream state.
    */
   public static final String WARN_UPSTREAM_NEVER = "never";
   
   /**
-   * A group for buttons so that will always be only one button selected.
-   */
-  private ButtonGroup group;
-  
-  /**
    * CheckBox for the option to notify the user as soon as there are new commits.
    */
-  private JCheckBox alwaysCheckBox;
+  private JRadioButton alwaysCheckBox;
   
   /**
    * CheckBox for the option to notify the user when there is a change in the opened files.
    */
-  private JCheckBox onChangeCheckBox;
+  private JRadioButton onChangeCheckBox;
   
   /**
    * CheckBock for the option to never notify the user when there are new commits.
    */
-  private JCheckBox neverCheckBox;
+  private JRadioButton neverCheckBox;
   
   /**
    * The OptionsManager instance
    */
-  private static final OptionsManager optionsManager = OptionsManager.getInstance();
+  private final OptionsManager optionsManager = OptionsManager.getInstance();
   
   /**
    * The Translator instance
    */
-  private static final Translator translator = Translator.getInstance();
+  private final Translator translator = Translator.getInstance();
   
   /**
    * @see ro.sync.exml.plugin.option.OptionPagePluginExtension#apply(ro.sync.exml.workspace.api.PluginWorkspace)
@@ -73,17 +68,14 @@ public class OxygenGitOptionPagePluginExtension extends OptionPagePluginExtensio
   
   @Override
   public void apply(PluginWorkspace pluginWorkspace) {
+    String warnOnUpstream = WARN_UPSTREAM_NEVER;
     if (alwaysCheckBox.isSelected()) {
-      optionsManager.setWarnOnUpstreamChange(WARN_UPSTREAM_ALWAYS);
-    } else {
-      if (onChangeCheckBox.isSelected()) {
-        optionsManager.setWarnOnUpstreamChange(WARN_UPSTREAM_ON_CHANGE);
-      } else {
-        if (neverCheckBox.isSelected()) {
-          optionsManager.setWarnOnUpstreamChange(WARN_UPSTREAM_NEVER);
-        }
-      }
-    }
+      warnOnUpstream = WARN_UPSTREAM_ALWAYS;
+    } else if (onChangeCheckBox.isSelected()) {
+      warnOnUpstream = WARN_UPSTREAM_ON_CHANGE;
+    } 
+    
+    optionsManager.setWarnOnUpstreamChange(warnOnUpstream);
     optionsManager.saveOptions();
   }
 
@@ -92,7 +84,6 @@ public class OxygenGitOptionPagePluginExtension extends OptionPagePluginExtensio
    */
   @Override
   public void restoreDefaults() {
-    // Reset the text fields values. Empty string is used to map the <null> default values of the options.
     neverCheckBox.setSelected(true);
   }
 
@@ -113,32 +104,35 @@ public class OxygenGitOptionPagePluginExtension extends OptionPagePluginExtensio
     JPanel panel = new JPanel(new GridBagLayout());
     
     //Add a label that asks us when do we want to see if there are remote changes for the repository.
-    JLabel whatcherTypeLbl = new JLabel("When do you want to be notified about new commits in the remote?");
+    // TODO i18n
+    JLabel whatcherTypeLbl = new JLabel("Notify about new commits in the remote:");
     c.gridx = 0;
     c.gridy = 0;
     c.weightx = 0;
     c.weighty = 0;
     c.anchor = GridBagConstraints.LINE_START;
+    c.insets = new Insets(0, 0, 5, 0);
     panel.add(whatcherTypeLbl, c);
     
     //Create a ButtonGroup so that there will be always only one option selected.
-    group = new ButtonGroup();
+    ButtonGroup group = new ButtonGroup();
 
     //Add the alwayCheckBox option that notifies us as soon as there are any remote changes.
     c.gridy ++;
-    alwaysCheckBox = new JCheckBox(translator.getTranslation(Tags.ALWAYS_NOTIFY_ON_NEW_COMMITS));
+    c.insets = new Insets(0, 0, 0, 0);
+    alwaysCheckBox = new JRadioButton(translator.getTranslation(Tags.ALWAYS_NOTIFY_ON_NEW_COMMITS));
     group.add(alwaysCheckBox);
     panel.add(alwaysCheckBox, c);
     
     //Add the onChangeCheckBox option that looks for remote changes in the files opened in the editing areas.
     c.gridy ++;
-    onChangeCheckBox = new JCheckBox(translator.getTranslation(Tags.NOTIFY_ON_POSSIBLE_CONFLICTS));
+    onChangeCheckBox = new JRadioButton(translator.getTranslation(Tags.NOTIFY_ON_POSSIBLE_CONFLICTS));
     group.add(onChangeCheckBox);
     panel.add(onChangeCheckBox, c);
     
     //Add the neverCheckBox option that never looks for remote changes.
     c.gridy ++;
-    neverCheckBox = new JCheckBox(translator.getTranslation(Tags.NEVER_NOTIFY_ON_NEW_COMMITS));
+    neverCheckBox = new JRadioButton(translator.getTranslation(Tags.NEVER_NOTIFY_ON_NEW_COMMITS));
     group.add(neverCheckBox);
     panel.add(neverCheckBox, c);
     
