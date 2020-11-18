f2c6b7790978c6ef45561dc993850755612fff83
AlexJitianu <jitianualex83@gmail.com>
2020-11-18 17:30:51 +0200

    EXM-44341 Fixed tests. The conflicts panel should listen for a STAGE event and check if the repo state changed.

--- src/test/java/com/oxygenxml/git/service/TestUtil.java
+++ src/test/java/com/oxygenxml/git/service/TestUtil.java
@@ -1,7 +1,7 @@
 package com.oxygenxml.git.service;
 
-import com.oxygenxml.git.view.event.GitEventInfo;
 import com.oxygenxml.git.view.event.GitController;
+import com.oxygenxml.git.view.event.GitEventInfo;
 import com.oxygenxml.git.view.event.PushPullEvent;
 
 public class TestUtil {
@@ -10,13 +10,19 @@ public class TestUtil {
       @Override
       public void operationAboutToStart(GitEventInfo changeEvent) {
         if (changeEvent instanceof PushPullEvent) {
-          b.append(changeEvent).append("\n");
+          b.append("Status: STARTED, message: ").append(((PushPullEvent) changeEvent).getMessage()).append("\n");
         }
       }
       @Override
       public void operationSuccessfullyEnded(GitEventInfo changeEvent) {
         if (changeEvent instanceof PushPullEvent) {
-          b.append(changeEvent).append("\n");
+          b.append("Status: FINISHED, message: ").append(((PushPullEvent) changeEvent).getMessage()).append("\n");
+        }
+      }
+      @Override
+      public void operationFailed(GitEventInfo info, Throwable t) {
+        if (info instanceof PushPullEvent) {
+          b.append("Status: FAILED, message: ").append(((PushPullEvent) info).getMessage()).append("\n");
         }
       }
     });
