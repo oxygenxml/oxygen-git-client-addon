f7abc0c91d71d0a3d5c17828c2c72106b65a76c5
Sorin Carbunaru <sorin_carbunaru@sync.ro>
2021-02-19 10:47:41 +0200

    EXM-47153: removed detached HEAD from list of local branches

--- src/main/java/com/oxygenxml/git/service/GitAccess.java
+++ src/main/java/com/oxygenxml/git/service/GitAccess.java
@@ -797,6 +797,16 @@ public class GitAccess {
 		List<Ref> branches = Collections.emptyList();
 		try {
 			branches = git.branchList().call();
+			// EXM-47153: if we are on a detached HEAD, 
+			// remove it from the list of local branches
+			Iterator<Ref> iterator = branches.iterator();
+			while (iterator.hasNext()) {
+			  Ref ref = iterator.next();
+			  if (Constants.HEAD.equals(ref.getName())) {
+			    iterator.remove();
+			    break;
+			  }
+			}
 		} catch (GitAPIException e) {
 		  logger.error(e, e);
 		}
