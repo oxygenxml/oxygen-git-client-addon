da0c55837f31fe53a04e8812b979aeeb8ada7bf0
Sorin Carbunaru <sorin_carbunaru@sync.ro>
2021-08-06 14:36:01 +0300

    minor changes

--- src/test/java/com/oxygenxml/git/view/staging/FlatView11Test.java
+++ src/test/java/com/oxygenxml/git/view/staging/FlatView11Test.java
@@ -43,7 +43,7 @@ public class FlatView11Test extends FlatViewTestBase {
    * @throws Exception
    */
   @Test
-  public void testAmendCommitThatWasPushed_changeFileContent() throws Exception {
+  public void testTruncateFilenames() throws Exception {
     // Create repositories
     String localTestRepository = "target/test-resources/testAmendCommitThatWasPushed_1_local";
     String testFolder = localTestRepository +"/test1/test2/test3/test4/test5/test6/test7/test8/test9/test10/test11/test12/test13/test14/test15";
@@ -53,17 +53,21 @@ public class FlatView11Test extends FlatViewTestBase {
     new File(testFolder).mkdirs();
     createNewFile(testFolder, "test.txt", "content");
 
-    SwingUtilities.invokeLater(() -> {
-      mainFrame.setSize(new Dimension(400, 400));
-      mainFrame.repaint();
-    });
-    String expectedPath = "test1/.../test9/test10/test11/test12/test13/test14/test15/test.txt";
     JTable filesTable = stagingPanel.getUnstagedChangesPanel().getFilesTable();
     DefaultTableCellRenderer tableCellRenderer = (DefaultTableCellRenderer) filesTable.getCellRenderer(0, 1)
         .getTableCellRendererComponent(filesTable, filesTable.getModel().getValueAt(0, 1), false, false, 0, 1);
-    String actualPath = tableCellRenderer.getText();
-    assertEquals(expectedPath, actualPath);
+    
+    // First size
+    SwingUtilities.invokeLater(() -> {
+      mainFrame.setSize(new Dimension(405, 400));
+      mainFrame.repaint();
+    });
+    flushAWT();
+    assertEquals(
+        "test1/.../test9/test10/test11/test12/test13/test14/test15/test.txt",
+        tableCellRenderer.getText());
 
+    // Shrink width
     SwingUtilities.invokeLater(() -> {
       mainFrame.setSize(new Dimension(320, 400));
       mainFrame.repaint();
@@ -73,6 +77,7 @@ public class FlatView11Test extends FlatViewTestBase {
         "test1/.../test11/test12/test13/test14/test15/test.txt",
         tableCellRenderer.getText());
 
+    // Shrink even more
     SwingUtilities.invokeLater(() -> {
       mainFrame.setSize(new Dimension(80, 400));
       mainFrame.repaint();
