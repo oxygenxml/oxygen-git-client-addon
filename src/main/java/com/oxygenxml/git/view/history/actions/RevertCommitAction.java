0bd0b9b2468affb29d5564a09e21052ba2d4b2c4
Sorin Carbunaru <sorin_carbunaru@sync.ro>
2021-08-09 17:14:14 +0300

    EXM-47154: code review

--- src/main/java/com/oxygenxml/git/view/history/actions/RevertCommitAction.java
+++ src/main/java/com/oxygenxml/git/view/history/actions/RevertCommitAction.java
@@ -1,6 +1,7 @@
 package com.oxygenxml.git.view.history.actions;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 
 import javax.swing.AbstractAction;
 
@@ -9,6 +10,7 @@ import org.apache.log4j.Logger;
 
 import com.oxygenxml.git.service.GitAccess;
 import com.oxygenxml.git.service.GitOperationScheduler;
+import com.oxygenxml.git.service.NoRepositorySelected;
 import com.oxygenxml.git.translator.Tags;
 import com.oxygenxml.git.translator.Translator;
 import com.oxygenxml.git.view.dialog.FileStatusDialog;
@@ -17,6 +19,10 @@ import com.oxygenxml.git.view.history.CommitCharacteristics;
 import ro.sync.exml.workspace.api.PluginWorkspaceProvider;
 import ro.sync.exml.workspace.api.standalone.ui.OKCancelDialog;
 
+/**
+ * Action that reverts all the changes from a commit
+ * and creates a new commit for the reverted state. 
+ */
 public class RevertCommitAction extends AbstractAction {
   /**
    * Logger for logging.
@@ -31,24 +37,29 @@ public class RevertCommitAction extends AbstractAction {
   /**
    * Constructor.
    * 
-   * @param commitId
+   * @param commitCharacteristics Commit characteristics.
    */
   public RevertCommitAction(CommitCharacteristics commitCharacteristics) {
     super(Translator.getInstance().getTranslation(Tags.REVERT_COMMIT));
     this.commitCharacteristics = commitCharacteristics;
   }
 
+  /**
+   * Action performed.
+   */
   @Override
   public void actionPerformed(ActionEvent e) {
     GitOperationScheduler.getInstance().schedule(() -> {
       try {
-        if ( FileStatusDialog.showQuestionMessage(Translator.getInstance().getTranslation(Tags.REVERT_COMMIT),
+        int result = FileStatusDialog.showQuestionMessage(
+            Translator.getInstance().getTranslation(Tags.REVERT_COMMIT),
             Translator.getInstance().getTranslation(Tags.REVERT_COMMIT_WARNING), 
-            Translator.getInstance().getTranslation(Tags.YES), Translator.getInstance().getTranslation(Tags.NO)) == OKCancelDialog.RESULT_OK) {
-          
-         GitAccess.getInstance().revertCommit(commitCharacteristics.getCommitId());
+            Translator.getInstance().getTranslation(Tags.YES),
+            Translator.getInstance().getTranslation(Tags.NO));
+        if ( result == OKCancelDialog.RESULT_OK) {
+          GitAccess.getInstance().revertCommit(commitCharacteristics.getCommitId());
         }
-      }  catch (Exception ex) {
+      } catch (NoRepositorySelected | IOException ex) {
         LOGGER.debug(ex);
         PluginWorkspaceProvider.getPluginWorkspace().showErrorMessage(ex.getMessage(), ex);
       }
