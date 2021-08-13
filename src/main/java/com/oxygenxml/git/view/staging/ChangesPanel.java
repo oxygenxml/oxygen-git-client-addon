a18fcb680cbfd2e4480a5a0c3c0854d899ef1ae9
Alexandru Smarandache <alex.smarandache@yahoo.com>
2021-08-13 17:39:12 +0300

    EXM-48559 Code Review

--- src/main/java/com/oxygenxml/git/view/staging/ChangesPanel.java
+++ src/main/java/com/oxygenxml/git/view/staging/ChangesPanel.java
@@ -1132,29 +1132,20 @@ public class ChangesPanel extends JPanel {
 
 	/**
 	 * @return <code>true</code> if there are conflicts in the selected files.
-	 * 
-	 * @author Alex_Smarandache
+	 *
 	 */
-	private boolean haveSelectedFilesConflicts() {
-	  boolean hasConflict = false;
+	private boolean isAnyConflictInSelection() {
+	  boolean hasConflict;
     if(currentViewMode == ResourcesViewMode.FLAT_VIEW) {
       List<FileStatus> selectedFilesTable = getTableSelectedFiles();
-      for (int i = 0; i < selectedFilesTable.size(); i++) {
-        if(selectedFilesTable.get(i).getChangeType() == GitChangeType.CONFLICT) {
-          hasConflict = true;
-          break;
-        }
-      }
+			hasConflict = selectedFilesTable.stream().anyMatch(
+							file -> file.getChangeType() == GitChangeType.CONFLICT);
     } else {
       List<String> selectedPaths = TreeUtil.getStringComonAncestor(tree);
       StagingResourcesTreeModel fileTreeModel = (StagingResourcesTreeModel) tree.getModel();
       List<FileStatus> fileStatusesForPaths = fileTreeModel.getFilesByPaths(selectedPaths);
-      for (int i = 0; i < fileStatusesForPaths.size(); i++) {
-        if(fileStatusesForPaths.get(i).getChangeType() == GitChangeType.CONFLICT) {
-          hasConflict = true;
-          break;
-        }
-      }
+			hasConflict = fileStatusesForPaths.stream().anyMatch(
+							file -> file.getChangeType() == GitChangeType.CONFLICT);
     }
     return hasConflict;
 	}
@@ -1164,11 +1155,12 @@ public class ChangesPanel extends JPanel {
 	 * something is selected in the current view(flat or tree)
 	 */
 	private void toggleSelectedButton() {
-	  if (changeSelectedButton != null) {	    
-	    boolean hasConflict = haveSelectedFilesConflicts();
-	    boolean isEnabled = 
-	        (currentViewMode == ResourcesViewMode.FLAT_VIEW && filesTable != null && filesTable.getSelectedRowCount() > 0
-	            || currentViewMode == ResourcesViewMode.TREE_VIEW && tree != null && tree.getSelectionCount() > 0) && !hasConflict;
+	  if (changeSelectedButton != null) {
+			boolean isAnyTableEntrySelected = currentViewMode == ResourcesViewMode.FLAT_VIEW && filesTable != null
+							&& filesTable.getSelectedRowCount() > 0;
+			boolean isAnyTreeItemSelected = currentViewMode == ResourcesViewMode.TREE_VIEW && tree != null
+							&& tree.getSelectionCount() > 0;
+	    boolean isEnabled = (isAnyTreeItemSelected || isAnyTableEntrySelected) && !isAnyConflictInSelection();
 	    changeSelectedButton.setEnabled(isEnabled);
 	  }
 	}
