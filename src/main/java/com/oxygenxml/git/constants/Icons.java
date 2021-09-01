585248139e16a30ca6b4bdf88eea339467b45047
Sorin Carbunaru <sorin_carbunaru@sync.ro>
2021-09-01 16:57:45 +0300

    EXM-46109: toolbar icon for Tags action - we load the icon from Oxygen

--- src/main/java/com/oxygenxml/git/constants/Icons.java
+++ src/main/java/com/oxygenxml/git/constants/Icons.java
@@ -131,14 +131,12 @@ public class Icons {
    * Unstage all.
    */
   public static final String UNSTAGE_ALL = "/images/UnstageAll16.png";
-  /**
-   * Tag icon
-   * 
-   * TODO: perhaps load EditProperties16.png from Oxygen
-   */
-  public static final String TAG = "/images/Tag16.png";
   
   //=============== These icons are loaded from Oxygen =================
+  /**
+   * Tag icon.
+   */
+  public static final String TAG = "/images/EditProperties16.png";
   /**
    * Info icon.
    */
