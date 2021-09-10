13052c64b07d3adb8ea75c2b32f002867389420d
Alexandru Smarandache <alex.smarandache@yahoo.com>
2021-09-10 17:34:15 +0300

    EXM-48768: CR

--- src/main/java/com/oxygenxml/git/view/branches/BranchManagementPanel.java
+++ src/main/java/com/oxygenxml/git/view/branches/BranchManagementPanel.java
@@ -122,10 +122,9 @@ public class BranchManagementPanel extends JPanel {
         if (operation == GitOperation.OPEN_WORKING_COPY
             || operation == GitOperation.CREATE_BRANCH
             || operation == GitOperation.CHECKOUT
+            || operation == GitOperation.DELETE_BRANCH
           ) {
-          BranchManagementPanel.this.refreshBranches();
-        } else if (operation == GitOperation.DELETE_BRANCH) {
-          SwingUtilities.invokeLater(() -> BranchManagementPanel.this.refreshBranches());
+          SwingUtilities.invokeLater(BranchManagementPanel.this::refreshBranches);
         }
       }
     });
