5018cc85439fa5a759e3e6af247afb14dfe87ef8
AdrianSorop <adrian_sorop@sync.ro>
2021-12-07 18:49:50 +0200

    EXM-49057 - code review

--- src/main/java/com/oxygenxml/git/view/tags/GitTagsManager.java
+++ src/main/java/com/oxygenxml/git/view/tags/GitTagsManager.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.ObjectId;
@@ -43,16 +45,17 @@ public class GitTagsManager {
    */
   public static List<String> getRemoteTagsTitle() throws GitAPIException{
     
-    List<String> remoteTagsTitles = new ArrayList<>(); 
     
     CredentialsProvider credentialsProvider = AuthUtil.getCredentialsProvider(GitAccess.getInstance().getHostName());
-    Collection <Ref> refs = GitAccess.getInstance().getGit().lsRemote().setRemote(
-    		GitAccess.getInstance().getRemoteFromCurrentBranch()).setCredentialsProvider(credentialsProvider).setTags(true).call();
-    for (Ref ref : refs) {
-      remoteTagsTitles.add(Repository.shortenRefName(ref.getName()));
-    }
+    Collection <Ref> refs = GitAccess.getInstance()
+        .getGit()
+        .lsRemote()
+        .setRemote(GitAccess.getInstance().getRemoteFromCurrentBranch())
+        .setCredentialsProvider(credentialsProvider)
+        .setTags(true)
+        .call();
     
-    return remoteTagsTitles;
+    return refs.stream().map(t -> Repository.shortenRefName(t.getName())).collect(Collectors.toList());
   }
   
   /**
@@ -178,13 +181,13 @@ public class GitTagsManager {
    */
   public static int getNoOfTags() throws GitAPIException {
 	  GitAccess gitAccess = GitAccess.getInstance();
-    List<Ref> refs = null;
     
+	  List<Ref> refs = null;
     if(gitAccess.isRepoInitialized()) {
     	refs = gitAccess.getGit().tagList().call();
     }
-    
-    return refs == null ? 0 : refs.size();
+
+    return Optional.ofNullable(refs).map(List<Ref>::size).orElse(0);
   }
   
 }
