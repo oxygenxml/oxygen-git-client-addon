9c1df42e1ff14ae48da9f4c326f6f597d32dd88b
Alexandru Smarandache <alex.smarandache@yahoo.com>
2022-05-04 16:03:06 +0300

    EXM-47776: added a file validator

---
+++ src/main/java/com/oxygenxml/git/validation/FilesValidator.java
@@ -0,0 +1,134 @@
+package com.oxygenxml.git.validation;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ro.sync.document.DocumentPositionedInfo;
+import ro.sync.exml.workspace.api.PluginWorkspace;
+import ro.sync.exml.workspace.api.PluginWorkspaceProvider;
+
+/**
+ * Used to validate files.
+ * 
+ * @author alex_smarandache
+ *
+ */
+public class FilesValidator implements IValidator {
+
+  /**
+   * Collector for all problems that occurs.
+   */
+  private ICollector collector;
+  
+  /**
+   * Logger for logging.
+   */
+  private static final Logger LOGGER = LoggerFactory.getLogger(FilesValidator.class);
+  
+  /**
+   * Method to validate.
+   */
+  private Method validateMethod;
+
+  /**
+   * The problems collector instance.
+   * 
+   * @see ro.sync.exml.workspace.api.util.validation.ValidatorProblemCollector.
+   */
+  private Object problemsCollector;
+
+  /**
+   * The validation util access.
+   */
+  private Object validationUtilAccess;
+  
+  /**
+   * <code>true</code> if the validation service is available.
+   */
+  private boolean isValidatorServiceAvailable = true;
+  
+  
+  /**
+   * Constructor.
+   */
+  public FilesValidator() {
+    this(new ProblemsCollector());
+  }
+  
+  /**
+   * Constructor.
+   * 
+   * @param collector The collector to collect all problems.
+   */
+  public FilesValidator(final ICollector collector) {
+    this.collector = collector;
+    extractResources();
+  }
+
+  /**
+   * Extract methods and classes needed, using reflexion.
+   */
+  //TODO Rewrite this after set minimum Oxygen version 25.0 
+  private void extractResources() {
+    try {
+      final PluginWorkspace pluginWorkspaceAccess = PluginWorkspaceProvider.getPluginWorkspace();
+      Method method = pluginWorkspaceAccess.getClass().getMethod("getValidationUtilAccess", new Class[0]);
+      validationUtilAccess = method.invoke(pluginWorkspaceAccess, new Object[0]);
+      Class valClass = Class.forName("ro.sync.exml.workspace.api.util.validation.ValidatorProblemCollector");
+      problemsCollector = Proxy.newProxyInstance(pluginWorkspaceAccess.getClass().getClassLoader(), new Class[] {
+          Class.forName("ro.sync.exml.workspace.api.util.validation.ValidatorProblemCollector")
+      }, new InvocationHandler() {
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+          if("problemsOccured".equals(method.getName())) {
+            DocumentPositionedInfo[] dpis = (DocumentPositionedInfo[]) args[0];
+            collector.add(dpis);
+          }
+          return null;
+        }
+      });
+      validateMethod = validationUtilAccess.getClass().getMethod("validateResources", Iterator.class, boolean.class, valClass);
+
+    } catch (Exception e) {
+      if(LOGGER.isDebugEnabled()) {
+        LOGGER.debug(e.getMessage(), e);
+      }
+      isValidatorServiceAvailable = false;
+    }
+  }
+
+  @Override
+  public void validate(final List<URL> files) {
+    if(isAvailable()) {
+      collector.reset();
+      try {
+        validateMethod.invoke(validationUtilAccess, new Object[] {
+            files.iterator(),
+            false,
+           problemsCollector
+        });
+      } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+        LOGGER.error(e.getMessage(), e);
+      }
+    }
+  } 
+  
+  @Override
+  public ICollector getCollector() {
+    return this.collector;
+  }
+
+  @Override
+  public boolean isAvailable() {
+    return this.isValidatorServiceAvailable;
+  }
+
+}
