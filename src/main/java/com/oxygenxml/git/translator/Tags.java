4e3c5c4c44a7dda4f556343b41f78412b637cbe5
Alexandru Smarandache <alex.smarandache@yahoo.com>
2022-05-06 02:26:37 +0300

    EXM-47776: Created validation manager

--- src/main/java/com/oxygenxml/git/translator/Tags.java
+++ src/main/java/com/oxygenxml/git/translator/Tags.java
@@ -1837,5 +1837,14 @@ public class Tags {
    */
   public static final String REJECT_COMMIT_ON_PROBLEMS = "Reject_Commit_On_Problems";
 
+  /**
+   * Title for problems occurred on a pre-commit validation and presented by a dialog/view.
+   */
+  public static final String PRE_COMMIT_VALIDATION = "Pre_Commit_Validation";
+  
+  /**
+   * Displayed message when problems on validation are detected.
+   */
+  public static final String FAILED_COMMIT_VALIDATION_MESSAGE = "Failed_Commit_Validation_Message";
 }
 
---
+++ src/main/java/com/oxygenxml/git/validation/ValidationManager.java
@@ -0,0 +1,129 @@
+package com.oxygenxml.git.validation;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+import com.oxygenxml.git.options.OptionsManager;
+import com.oxygenxml.git.translator.Tags;
+import com.oxygenxml.git.translator.Translator;
+import com.oxygenxml.git.view.dialog.MessagePresenterProvider;
+import com.oxygenxml.git.view.dialog.internal.DialogType;
+
+import ro.sync.document.DocumentPositionedInfo;
+import ro.sync.exml.workspace.api.PluginWorkspaceProvider;
+import ro.sync.exml.workspace.api.results.ResultsManager;
+import ro.sync.exml.workspace.api.results.ResultsManager.ResultType;
+import ro.sync.exml.workspace.api.standalone.ui.OKCancelDialog;
+
+/**
+ * Manage the validations.
+ * 
+ * @author alex_smarandache
+ *
+ */
+public class ValidationManager {
+
+  /**
+   * The commit files validatior.
+   */
+  private IValidator commitFilesValidator;
+  
+  /**
+   * The translator for the messages that are displayed in this panel
+   */
+  private static final Translator TRANSLATOR = Translator.getInstance();
+  
+  /**
+   * Manager for options.
+   */
+  private static final OptionsManager OPTIONS_MANAGER = OptionsManager.getInstance();
+  
+  /**
+   * The result manager.
+   */
+  private static final ResultsManager RESULT_MANAGER = PluginWorkspaceProvider.getPluginWorkspace().getResultsManager();
+
+  
+  /**
+   * Hidden constructor.
+   */
+  private ValidationManager() {
+    commitFilesValidator = new FilesValidator(new ProblemsCollector());
+  }
+  
+  /**
+   * Helper class to manage the singleton instance.
+   *
+   * @author Alex_Smarandache
+   */
+  private static class SingletonHelper {
+      static final ValidationManager INSTANCE = new ValidationManager();
+  }
+
+  /**
+   * Get the unique instance.
+   *
+   * @return The instance.
+   */
+  public static ValidationManager getInstance() {
+      return SingletonHelper.INSTANCE;
+  }
+
+  /**
+   * @param filesValidator The new validator for files.
+   */
+  public void setFilesValidator(final IValidator filesValidator) {
+    this.commitFilesValidator = filesValidator;
+  }
+  
+  /**
+   * @return <code>true</code> if the files can and should be validated before commit.
+   */
+  public boolean validateFilesBeforeCommit() {
+    return OPTIONS_MANAGER.isFilesValidatedBeforeCommit() && commitFilesValidator.isAvailable();
+  }
+  
+  /**
+   * Validate files before to commit.
+   * 
+   * @param files The files to be validated.
+   *  
+   * @return <code>true</code> if no problems are found.
+   */
+  public boolean validateFilesBeforeCommit(final List<URL> files) {
+    commitFilesValidator.validate(files);
+    return commitFilesValidator.getCollector().isEmpty();
+  }
+  
+  /**
+   * Show problems that occurs on last commit validation.
+   * 
+   * @return <code>true</code> if the commit can be performed.
+   */
+  public boolean showCommitFilesProblems() {
+    final List<DocumentPositionedInfo> problems = Arrays.asList(
+        commitFilesValidator.getCollector().getAll());
+    RESULT_MANAGER.setResults(TRANSLATOR.getTranslation(Tags.PRE_COMMIT_VALIDATION), 
+        problems, ResultType.PROBLEM);
+    boolean toReturn = false;
+    if(OPTIONS_MANAGER.isCommitRejectedOnValidationProblems()) {
+      MessagePresenterProvider.getBuilder(TRANSLATOR.getTranslation(
+          Tags.PRE_COMMIT_VALIDATION), DialogType.ERROR)
+      .setOkButtonVisible(false)
+      .setCancelButtonName(Translator.getInstance().getTranslation(Tags.CLOSE))
+      .setMessage(TRANSLATOR.getTranslation(Tags.FAILED_COMMIT_VALIDATION_MESSAGE))
+      .buildAndShow();
+    } else {
+      toReturn = MessagePresenterProvider.getBuilder(TRANSLATOR.getTranslation(
+          Tags.PRE_COMMIT_VALIDATION), DialogType.ERROR)
+      .setMessage(TRANSLATOR.getTranslation(Tags.FAILED_COMMIT_VALIDATION_MESSAGE))
+      .setOkButtonName(Tags.COMMIT_ANYWAY)
+      .setCancelButtonName(Translator.getInstance().getTranslation(Tags.CLOSE))
+      .buildAndShow().getResult() == OKCancelDialog.RESULT_OK;
+    }
+    
+    return toReturn;
+  }
+  
+}
