09fd659c74da0ff9b499ecec8f86eee554f18924
Alexandru Smarandache <alex.smarandache@yahoo.com>
2022-05-18 13:44:09 +0300

    EXM-50426: Added methods in validation manager for pre-push validation

--- src/main/java/com/oxygenxml/git/validation/ValidationManager.java
+++ src/main/java/com/oxygenxml/git/validation/ValidationManager.java
@@ -1,24 +1,38 @@
 package com.oxygenxml.git.validation;
 
+import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.oxygenxml.git.ProjectHelper;
 import com.oxygenxml.git.options.OptionsManager;
 import com.oxygenxml.git.service.GitAccess;
+import com.oxygenxml.git.service.GitOperationScheduler;
 import com.oxygenxml.git.service.annotation.TestOnly;
 import com.oxygenxml.git.service.entities.FileStatusUtil;
 import com.oxygenxml.git.service.entities.GitChangeType;
 import com.oxygenxml.git.translator.Tags;
 import com.oxygenxml.git.translator.Translator;
+import com.oxygenxml.git.utils.RepoUtil;
 import com.oxygenxml.git.view.dialog.MessagePresenterProvider;
 import com.oxygenxml.git.view.dialog.internal.DialogType;
+import com.oxygenxml.git.view.util.CollectionsUtil;
 
 import ro.sync.document.DocumentPositionedInfo;
+import ro.sync.exml.workspace.api.PluginWorkspace;
 import ro.sync.exml.workspace.api.PluginWorkspaceProvider;
 import ro.sync.exml.workspace.api.results.ResultsManager;
 import ro.sync.exml.workspace.api.results.ResultsManager.ResultType;
+import ro.sync.exml.workspace.api.standalone.StandalonePluginWorkspace;
 import ro.sync.exml.workspace.api.standalone.ui.OKCancelDialog;
 
 /**
@@ -30,40 +44,51 @@ import ro.sync.exml.workspace.api.standalone.ui.OKCancelDialog;
 public class ValidationManager {
 
   /**
-   * The commit files validatior.
+   * The commit files validator.
    */
   private IValidator commitFilesValidator;
+
+  /**
+   * The push main files validator.
+   */
+  private IValidator pushMainFilesValidator;
   
+  /**
+   * Logger for logging.
+   */
+  private static final Logger LOGGER = LoggerFactory.getLogger(ValidationManager.class);
+
   /**
    * The translator for the messages that are displayed in this panel
    */
   private static final Translator TRANSLATOR = Translator.getInstance();
-  
+
   /**
    * Manager for options.
    */
   private static final OptionsManager OPTIONS_MANAGER = OptionsManager.getInstance();
-  
+
   /**
    * The result manager.
    */
   private static final ResultsManager RESULT_MANAGER = PluginWorkspaceProvider.getPluginWorkspace().getResultsManager();
 
-  
+
   /**
    * Hidden constructor.
    */
   private ValidationManager() {
-    commitFilesValidator = new FilesValidator(new ProblemsCollector());
+    commitFilesValidator   = new FilesValidator(new ProblemsCollector());
+    pushMainFilesValidator = new FilesValidator(new ProblemsCollector());
   }
-  
+
   /**
    * Helper class to manage the singleton instance.
    *
    * @author Alex_Smarandache
    */
   private static class SingletonHelper {
-      static final ValidationManager INSTANCE = new ValidationManager();
+    static final ValidationManager INSTANCE = new ValidationManager();
   }
 
   /**
@@ -72,15 +97,23 @@ public class ValidationManager {
    * @return The instance.
    */
   public static ValidationManager getInstance() {
-      return SingletonHelper.INSTANCE;
+    return SingletonHelper.INSTANCE;
   }
 
   /**
-   * @param filesValidator The new validator for files.
+   * @param preCommitFilesValidator The new pre-commit validator.
    */
   @TestOnly
-  public void setFilesValidator(final IValidator filesValidator) {
-    this.commitFilesValidator = filesValidator;
+  public void setPreCommitFilesValidator(final IValidator preCommitFilesValidator) {
+    this.commitFilesValidator = preCommitFilesValidator;
+  }
+
+  /**
+   * @param prePushFilesValidator The new pre-push validator.
+   */
+  @TestOnly
+  public void setPrePushFilesValidator(final IValidator prePushFilesValidator) {
+    this.pushMainFilesValidator = prePushFilesValidator;
   }
   
   /**
@@ -89,14 +122,14 @@ public class ValidationManager {
   public boolean isPreCommitValidationEnabled() {
     return OPTIONS_MANAGER.isFilesValidatedBeforeCommit() && isAvailable();
   }
-  
+
   /**
    * @return <code>true</code> if the validation support is available(for Oxygen 25 and newer).
    */
   public boolean isAvailable() {
     return commitFilesValidator.isAvailable();
   }
-  
+
   /**
    * Validate files before to commit.
    * 
@@ -108,7 +141,19 @@ public class ValidationManager {
     commitFilesValidator.validate(files);
     return commitFilesValidator.getCollector().isEmpty();
   }
-  
+
+  /**
+   * Validate main files before to push.
+   * 
+   * @param files The files to be validated.
+   *  
+   * @return <code>true</code> if no problems are found.
+   */
+  public boolean validateMainFilesBeforePush(final List<URL> files) {
+    pushMainFilesValidator.validate(files);
+    return pushMainFilesValidator.getCollector().isEmpty();
+  }
+
   /**
    * Show problems that occurs on last commit validation.
    * 
@@ -130,15 +175,15 @@ public class ValidationManager {
     } else {
       toReturn = MessagePresenterProvider.getBuilder(TRANSLATOR.getTranslation(
           Tags.PRE_COMMIT_VALIDATION), DialogType.ERROR)
-      .setMessage(TRANSLATOR.getTranslation(Tags.FAILED_COMMIT_VALIDATION_MESSAGE))
-      .setOkButtonName(Tags.COMMIT_ANYWAY)
-      .setCancelButtonName(TRANSLATOR.getTranslation(Tags.CLOSE))
-      .buildAndShow().getResult() == OKCancelDialog.RESULT_OK;
+          .setMessage(TRANSLATOR.getTranslation(Tags.FAILED_COMMIT_VALIDATION_MESSAGE))
+          .setOkButtonName(Tags.COMMIT_ANYWAY)
+          .setCancelButtonName(TRANSLATOR.getTranslation(Tags.CLOSE))
+          .buildAndShow().getResult() == OKCancelDialog.RESULT_OK;
     }
-    
+
     return toReturn;
   }
-  
+
   /**
    * Check all staged files without missed or removed files.
    * <br>
@@ -152,16 +197,145 @@ public class ValidationManager {
     boolean performCommit = true;
     if(isPreCommitValidationEnabled() 
         && !validateFilesBeforeCommit(
-        FileStatusUtil.getFilesStatuesURL(
-            GitAccess.getInstance().getStagedFiles().stream().filter(
-                file -> file.getChangeType() != GitChangeType.REMOVED && 
-                file.getChangeType() != GitChangeType.MISSING)
-            .collect(Collectors.toList()))
-        )) {
+            FileStatusUtil.getFilesStatuesURL(
+                GitAccess.getInstance().getStagedFiles().stream().filter(
+                    file -> file.getChangeType() != GitChangeType.REMOVED && 
+                    file.getChangeType() != GitChangeType.MISSING)
+                .collect(Collectors.toList()))
+            )) {
       performCommit = showCommitFilesProblems();
     }
-    
+
     return performCommit;
+  }
+
+  /**
+   * @return <code>true</code> if the pre push validation is enabled.
+   */
+  public boolean isPrePushValidationEnabled() {
+    return OPTIONS_MANAGER.isMainFilesValidatedBeforePush();
+  }
+
+  /**
+   * Check all main files to test if the project is valid or no.
+   * <br>
+   * If problems are detected, are presented for a user confirmation if the push should be canceled or performed.
+   * 
+   * @see GitChangeType
+   * 
+   * @return <code>true</code> if the project is valid and the push should be performed.
+   */
+  public boolean checkPushValid() {
+    boolean performPush = true;
+    final PluginWorkspace pluginWorkspace = PluginWorkspaceProvider.getPluginWorkspace();
+    if(isPrePushValidationEnabled() && pluginWorkspace instanceof StandalonePluginWorkspace) {
+      final StandalonePluginWorkspace standalonePluginWorkspace = (StandalonePluginWorkspace)pluginWorkspace;
+      final URL currentProjectURL = standalonePluginWorkspace.getProjectManager().getCurrentProjectURL(); 
+      Optional<RevCommit> stash = Optional.empty();
+      try {
+        if(currentProjectURL == null ||!RepoUtil.isEqualsWithCurrentRepo(new File(currentProjectURL.toURI()))) {
+          performPush = treatNotSameProjectCase(standalonePluginWorkspace);
+        } 
+        
+        // treat case with validation could not be performed because there are uncommited changes.
+        if(performPush && GitAccess.getInstance().hasFilesChanged()) { 
+          performPush = MessagePresenterProvider
+              .getBuilder(TRANSLATOR.getTranslation(Tags.PRE_PUSH_VALIDATION), DialogType.WARNING)
+              .setQuestionMessage(TRANSLATOR.getTranslation(Tags.PUSH_VALIDATION_UNCOMMITED_CHANGES))
+              .setOkButtonName(TRANSLATOR.getTranslation(Tags.STASH))
+              .buildAndShow().getResult() == OKCancelDialog.RESULT_OK;
+          if(performPush) {
+            stash = Optional.ofNullable(GitAccess.getInstance().createStash(true, ""));
+          }
+        }
+        
+        if(performPush && !validateMainFilesBeforePush(CollectionsUtil.toList(
+            OxygenAPIWrapper.getInstance().getMainFileResourcesIterator()))) {
+          performPush = showPushFilesProblems(); 
+        }
+      } catch (URISyntaxException e) {
+        standalonePluginWorkspace.showErrorMessage(e.getMessage());
+        performPush = false;
+      } finally {
+        removeStashIfNeeded(stash);
+      }
+    }   
+
+    return performPush;
+  }
+
+  /**
+   * This case occurs when the project loaded "Project" View is not the same with project loaded in Git Staging.
+   * 
+   * @param standalonePluginWorkspace The Standalone Plugin Workspace.
+   * 
+   * @return <code>true</code> if the push can be performed.
+   */
+  private boolean treatNotSameProjectCase(final StandalonePluginWorkspace standalonePluginWorkspace) {
+    boolean performPush = MessagePresenterProvider 
+        .getBuilder(TRANSLATOR.getTranslation(Tags.PRE_PUSH_VALIDATION), DialogType.WARNING)
+        .setQuestionMessage(TRANSLATOR.getTranslation(Tags.NOT_SAME_PROJECT_MESSAGE))
+        .setOkButtonName(TRANSLATOR.getTranslation(Tags.LOAD))
+        .buildAndShow().getResult() == OKCancelDialog.RESULT_OK;
+    if(performPush) {
+      final Optional<File> currentProjectXprFile = Optional.ofNullable(
+          ProjectHelper.getInstance().findXPRFromCurrentGitProject());
+      performPush = currentProjectXprFile.isPresent();
+      if(performPush) {
+        standalonePluginWorkspace.getProjectManager().loadProject(currentProjectXprFile.get());
+      } else {
+        standalonePluginWorkspace.showErrorMessage(TRANSLATOR.getTranslation(Tags.NO_XPR_FILE_FOUND_MESSAGE));
+      }
+    }
+    return performPush;
+  }
+
+  /**
+   * This method remove the stash(if this exists) created to store uncommited changes.
+   * 
+   * @param stash The created stash or empty @Optional if no stash were created.
+   */
+  private void removeStashIfNeeded(final Optional<RevCommit> stash) {
+    stash.ifPresent(st -> 
+      GitOperationScheduler.getInstance().schedule(
+        () -> {
+          try {
+            GitAccess.getInstance().popStash(st.toObjectId().getName());
+          } catch (GitAPIException e) {
+            LOGGER.error(e.getMessage(), e);
+          }
+        })
+    );
+  }
+
+  /**
+   * Show problems that occurs on last commit validation.
+   * 
+   * @return <code>true</code> if the commit can be performed.
+   */
+  public boolean showPushFilesProblems() {
+    final List<DocumentPositionedInfo> problems = Arrays.asList(
+        pushMainFilesValidator.getCollector().getAll());
+    RESULT_MANAGER.setResults(TRANSLATOR.getTranslation(Tags.PRE_PUSH_VALIDATION), 
+        problems, ResultType.PROBLEM);
+    boolean toReturn = false;
+    if(OPTIONS_MANAGER.isPushRejectedOnValidationProblems()) {
+      MessagePresenterProvider.getBuilder(TRANSLATOR.getTranslation(
+          Tags.PRE_PUSH_VALIDATION), DialogType.ERROR)
+      .setOkButtonVisible(false)
+      .setCancelButtonName(TRANSLATOR.getTranslation(Tags.CLOSE))
+      .setMessage(TRANSLATOR.getTranslation(Tags.PUSH_VALIDATION_FAILED))
+      .buildAndShow();
+    } else {
+      toReturn = MessagePresenterProvider.getBuilder(TRANSLATOR.getTranslation(
+          Tags.PRE_PUSH_VALIDATION), DialogType.ERROR)
+          .setMessage(TRANSLATOR.getTranslation(Tags.FAILED_COMMIT_VALIDATION_MESSAGE))
+          .setOkButtonName(TRANSLATOR.getTranslation(Tags.PUSH_ANYWAY))
+          .setCancelButtonName(TRANSLATOR.getTranslation(Tags.CLOSE))
+          .buildAndShow().getResult() == OKCancelDialog.RESULT_OK;
+    }
+
+    return toReturn;
   }
   
 }
