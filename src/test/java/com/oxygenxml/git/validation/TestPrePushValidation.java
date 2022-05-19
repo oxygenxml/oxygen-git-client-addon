66fdb4814af248e9eb933c8d8d400af9e38f6a27
Alexandru Smarandache <alex.smarandache@yahoo.com>
2022-05-19 14:00:51 +0300

    EXM-50426: added TC for case when the project should be not validated because there are uncommited changes

--- src/test/java/com/oxygenxml/git/validation/TestPrePushValidation.java
+++ src/test/java/com/oxygenxml/git/validation/TestPrePushValidation.java
@@ -253,6 +253,95 @@ public class TestPrePushValidation extends GitTestBase {
     assertTrue(dialogPresentedFlags[1]);
     assertTrue(dialogPresentedFlags[2]);
   }
+  
+  /**
+   * <p><b>Description:</b> This test cover pre-push validation behavior for case when this option
+   * is enabled and the push should not be rejected on validation problem and there are uncommited changes</p>
+   * 
+   * <p><b>Bug ID:</b> EXM-50426</p>
+   *
+   * @author Alex_Smarandache
+   *
+   */ 
+  @Test
+  public void testPrePushValidationWhenUncommitedChangesExist() throws Exception {
+    OptionsManager.getInstance().setValidateMainFilesBeforePush(true);
+    OptionsManager.getInstance().setRejectPushOnValidationProblems(false);
+    
+    StandalonePluginWorkspace spw =  (StandalonePluginWorkspace) PluginWorkspaceProvider.getPluginWorkspace();
+    final ProjectController projectController = Mockito.mock(ProjectController.class);
+    Mockito.when(projectController.getCurrentProjectURL()).thenReturn(
+        secondLocalRepo.getDirectory().toURI().toURL());
+    Mockito.when(spw.getProjectManager()).thenReturn(projectController);
+    // Create a custom dialog to return a custom result. Usefully to simulate a dialog showing.
+    final int[] dialogResult = new int[1];
+    dialogResult[0] = OKCancelDialog.RESULT_OK;
+    final MessageDialog dialog = Mockito.mock(MessageDialog.class);
+    Mockito.when(dialog.getResult()).then((Answer<Integer>) 
+        invocation -> {
+          return dialogResult[0];
+        }); 
+
+    // Detects if the "Stash" button is available and the dialog is shows.
+    final boolean[] dialogPresentedFlags = new boolean[3];
+    dialogPresentedFlags[0] = false;
+    dialogPresentedFlags[1] = false;
+    dialogPresentedFlags[2] = false;
+
+    MessagePresenterProvider.setBuilder(new MessageDialogBuilder(
+        "test_push", DialogType.ERROR) {
+     
+      @Override
+          public MessageDialogBuilder setQuestionMessage(String questionMessage) {
+            dialogPresentedFlags[2] = dialogPresentedFlags[2] ? true : Tags.PUSH_VALIDATION_UNCOMMITED_CHANGES.equals(questionMessage);
+            return super.setQuestionMessage(questionMessage);
+          }
+      
+      @Override
+      public MessageDialogBuilder setOkButtonName(String okButtonName) {
+        dialogPresentedFlags[1] = dialogPresentedFlags[1] ? true : Tags.STASH.equals(okButtonName);
+        return super.setOkButtonName(okButtonName);
+      }
+
+      @Override
+      public MessageDialog buildAndShow() {
+        dialogPresentedFlags[0] = true;
+        return dialog;
+      }
+    });
+
+    // Create a custom collector constructed to behave as if it contains validation problems
+    final ICollector collector = Mockito.mock(ICollector.class);
+    Mockito.when(collector.isEmpty()).then((Answer<Boolean>) 
+        invocation -> {
+          return true;
+        });
+    Mockito.when(collector.getAll()).then((Answer<DocumentPositionedInfo[]>) 
+        invocation -> {
+          return new DocumentPositionedInfo[0];
+        }); 
+
+    // A custom validator that is always available and return the custom collector created before
+    final IValidator validator = Mockito.mock(IValidator.class);
+    Mockito.when(validator.isAvailable()).then((Answer<Boolean>) 
+        invocation -> {
+          return true;
+        });
+    Mockito.when(validator.getCollector()).then((Answer<ICollector>) 
+        invocation -> {
+          return collector;
+        });
+
+    ValidationManager.getInstance().setPrePushFilesValidator(validator);
+   
+    assertTrue(PluginWorkspaceProvider.getPluginWorkspace() instanceof StandalonePluginWorkspace);
+    assertTrue(gitAccess.hasFilesChanged());
+    assertTrue(ValidationManager.getInstance().isPrePushValidationEnabled());
+    assertTrue(ValidationManager.getInstance().checkPushValid());
+    assertTrue(dialogPresentedFlags[0]);
+    assertTrue(dialogPresentedFlags[1]);
+    assertTrue(dialogPresentedFlags[2]);
+  }
 
   @Override
   public void tearDown() throws Exception {
