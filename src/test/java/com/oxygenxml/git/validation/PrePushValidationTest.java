7c89d104564c58b7afd3bf9895c97df8d2180638
Alexandru Smarandache <alex.smarandache@yahoo.com>
2022-06-07 00:55:56 +0300

    EXM-50639: update TC

--- src/test/java/com/oxygenxml/git/validation/PrePushValidationTest.java
+++ src/test/java/com/oxygenxml/git/validation/PrePushValidationTest.java
@@ -4,8 +4,11 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
+import org.eclipse.jgit.lib.ConfigConstants;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.StoredConfig;
+import org.eclipse.jgit.transport.RefSpec;
 import org.eclipse.jgit.transport.RemoteConfig;
 import org.eclipse.jgit.transport.URIish;
 import org.junit.Test;
@@ -15,7 +18,6 @@ import org.mockito.stubbing.Answer;
 import com.oxygenxml.git.options.OptionsManager;
 import com.oxygenxml.git.service.GitAccess;
 import com.oxygenxml.git.service.GitTestBase;
-import com.oxygenxml.git.service.RepoNotInitializedException;
 import com.oxygenxml.git.service.entities.FileStatus;
 import com.oxygenxml.git.service.entities.GitChangeType;
 import com.oxygenxml.git.translator.Tags;
@@ -27,6 +29,7 @@ import com.oxygenxml.git.view.dialog.MessagePresenterProvider;
 import com.oxygenxml.git.view.dialog.internal.DialogType;
 import com.oxygenxml.git.view.dialog.internal.MessageDialog;
 import com.oxygenxml.git.view.dialog.internal.MessageDialogBuilder;
+import com.oxygenxml.git.view.event.PullType;
 
 import ro.sync.document.DocumentPositionedInfo;
 import ro.sync.exml.workspace.api.PluginWorkspaceProvider;
@@ -45,17 +48,17 @@ public class PrePushValidationTest extends GitTestBase {
   /**
    * The first local repository path.
    */
-  private final static String FIRST_LOCAL_TEST_REPOSITPRY = "target/test-resources/GitAccessPushTest/local";
+  private final static String FIRST_LOCAL_TEST_REPOSITORY = "target/test-resources/PrePushValidationTest/local";
 
   /**
    * A remote repository.
    */
-  private final static String REMOTE_TEST_REPOSITPRY = "target/test-resources/GitAccessPushTest/remote";
+  private final static String REMOTE_TEST_REPOSITORY = "target/test-resources/PrePushValidationTest/remote";
 
   /**
    * The second test repository path.
    */
-  private final static String SECOND_LOCAL_TEST_REPOSITPRY = "target/test-resources/GitAccessPushTest/local2";
+  private final static String SECOND_LOCAL_TEST_REPOSITORY = "target/test-resources/PrePushValidationTest/local2";
 
   /**
    * The first local repository.
@@ -82,31 +85,162 @@ public class PrePushValidationTest extends GitTestBase {
     super.setUp();
 
     gitAccess = GitAccess.getInstance();
-    gitAccess.createNewRepository(REMOTE_TEST_REPOSITPRY);
+    gitAccess.createNewRepository(REMOTE_TEST_REPOSITORY);
     remoteRepo = gitAccess.getRepository();
-    gitAccess.createNewRepository(FIRST_LOCAL_TEST_REPOSITPRY);
+    gitAccess.createNewRepository(FIRST_LOCAL_TEST_REPOSITORY);
     firstLocalRepo = gitAccess.getRepository();
-    gitAccess.createNewRepository(SECOND_LOCAL_TEST_REPOSITPRY);
+    gitAccess.createNewRepository(SECOND_LOCAL_TEST_REPOSITORY);
     secondLocalRepo = gitAccess.getRepository();
 
-    File file = new File(FIRST_LOCAL_TEST_REPOSITPRY + "/test.xpr");
+    File file = new File(FIRST_LOCAL_TEST_REPOSITORY + "/test.xpr");
     try {
       file.createNewFile();
-      file = new File(SECOND_LOCAL_TEST_REPOSITPRY + "/test2.xpr");
+      file = new File(SECOND_LOCAL_TEST_REPOSITORY + "/test2.xpr");
       file.createNewFile();
     } catch (IOException e) {
       e.printStackTrace();
     }
-
-    gitAccess.setRepositorySynchronously(FIRST_LOCAL_TEST_REPOSITPRY);
-    final StoredConfig config = gitAccess.getRepository().getConfig();
-    final RemoteConfig remoteConfig = new RemoteConfig(config, "origin");
-    final URIish uri = new URIish(remoteRepo.getDirectory().toURI().toURL());
+    
+    StoredConfig config = firstLocalRepo.getConfig();
+    RemoteConfig remoteConfig = new RemoteConfig(config, "origin");
+    URIish uri = new URIish(remoteRepo.getDirectory().toURI().toURL());
     remoteConfig.addURI(uri);
+    RefSpec spec = new RefSpec("+refs/heads/*:refs/remotes/origin/*");
+    remoteConfig.addFetchRefSpec(spec);
     remoteConfig.update(config);
     config.save();
-
+    
+    gitAccess.setRepositorySynchronously(REMOTE_TEST_REPOSITORY);
+    File local1File = new File(REMOTE_TEST_REPOSITORY, "test1.txt");
+    local1File.createNewFile();
+    gitAccess.add(new FileStatus(GitChangeType.ADD, "test1.txt"));
+    gitAccess.commit("First");
+ 
+    gitAccess.setRepositorySynchronously(FIRST_LOCAL_TEST_REPOSITORY);
+    pull("", "", PullType.MERGE_FF, false);
+    
+    String branchName = GitAccess.DEFAULT_BRANCH_NAME;
+    String remoteName = "origin";
+    config.setString(ConfigConstants.CONFIG_BRANCH_SECTION, branchName,  ConfigConstants.CONFIG_KEY_REMOTE, remoteName);
+    config.setString(ConfigConstants.CONFIG_BRANCH_SECTION, branchName, ConfigConstants.CONFIG_KEY_MERGE, Constants.R_HEADS + branchName);
+    config.save();
+   
+    bindLocalToRemote(firstLocalRepo , remoteRepo);
+    sleep(500);
+    
+    pushOneFileToRemote(FIRST_LOCAL_TEST_REPOSITORY, "test_second_local.txt", "hellllo");
+    flushAWT();
   }
+  
+  @Override
+  public void tearDown() throws Exception {
+    super.tearDown();
+
+    gitAccess.closeRepo();
+    flushAWT();
+    firstLocalRepo.close();
+    flushAWT();
+    remoteRepo.close();
+    flushAWT();
+    secondLocalRepo.close();
+    flushAWT();
+    try {
+      File dirToDelete = new File(FIRST_LOCAL_TEST_REPOSITORY);
+      FileUtils.deleteDirectory(dirToDelete);
+      flushAWT();
+      dirToDelete = new File(REMOTE_TEST_REPOSITORY);
+      FileUtils.deleteDirectory(dirToDelete);
+      flushAWT();
+      dirToDelete = new File(SECOND_LOCAL_TEST_REPOSITORY);
+      FileUtils.deleteDirectory(dirToDelete);
+      flushAWT();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  /**
+   * <p><b>Description:</b> This test cover pre-push validation behavior for case when this option
+   * is enabled and the push should not be rejected on validation problem.</p>
+   * 
+   * <p><b>Bug ID:</b> EXM-50426</p>
+   *
+   * @author Alex_Smarandache
+   *
+   */ 
+  @Test
+  public void testPrePushValidationWhenPushIsNotRejected() throws Exception {
+    OptionsManager.getInstance().setValidateMainFilesBeforePush(true);
+    OptionsManager.getInstance().setRejectPushOnValidationProblems(false);
+    
+    final StandalonePluginWorkspace spw =  (StandalonePluginWorkspace) PluginWorkspaceProvider.getPluginWorkspace();
+    final ProjectController projectController = Mockito.mock(ProjectController.class);
+    Mockito.when(projectController.getCurrentProjectURL()).thenReturn(
+        firstLocalRepo.getDirectory().toURI().toURL());
+    Mockito.when(spw.getProjectManager()).thenReturn(projectController);
+    // Create a custom dialog to return a custom result. Usefully to simulate a dialog showing.
+    final int[] dialogResult = new int[1];
+    dialogResult[0] = OKCancelDialog.RESULT_OK;
+    final MessageDialog dialog = Mockito.mock(MessageDialog.class);
+    Mockito.when(dialog.getResult()).then((Answer<Integer>) 
+        invocation -> {
+          return dialogResult[0];
+        }); 
+
+    // Detects if the "Push anyway" button is available and the dialog is shows.
+    final boolean[] dialogPresentedFlags = new boolean[2];
+    dialogPresentedFlags[0] = false;
+    dialogPresentedFlags[1] = false;
+
+    MessagePresenterProvider.setBuilder(new MessageDialogBuilder(
+        "test_push", DialogType.ERROR) {
+
+      @Override
+      public MessageDialogBuilder setOkButtonName(String okButtonName) {
+        dialogPresentedFlags[1] = Tags.PUSH_ANYWAY.equals(okButtonName);
+        return super.setOkButtonName(okButtonName);
+      }
+
+      @Override
+      public MessageDialog buildAndShow() {
+        dialogPresentedFlags[0] = true;
+        return dialog;
+      }
+    });
+
+    // Create a custom collector constructed to behave as if it contains validation problems
+    final ICollector collector = Mockito.mock(ICollector.class);
+    Mockito.when(collector.isEmpty()).then((Answer<Boolean>) 
+        invocation -> {
+          return false;
+        });
+    Mockito.when(collector.getAll()).then((Answer<DocumentPositionedInfo[]>) 
+        invocation -> {
+          return new DocumentPositionedInfo[0];
+        }); 
+
+    // A custom validator that is always available and return the custom collector created before
+    final IValidator validator = Mockito.mock(IValidator.class);
+    Mockito.when(validator.isAvailable()).then((Answer<Boolean>) 
+        invocation -> {
+          return true;
+        });
+    Mockito.when(validator.getCollector()).then((Answer<ICollector>) 
+        invocation -> {
+          return collector;
+        });
+
+    ValidationManager.getInstance().setPrePushValidator(new PrePushValidation(validator, null));
+    gitAccess.add(new FileStatus(GitChangeType.ADD, "test.txt"));
+    gitAccess.commit("file test added");
+   
+    assertTrue(PluginWorkspaceProvider.getPluginWorkspace() instanceof StandalonePluginWorkspace);
+    assertTrue(ValidationManager.getInstance().isPrePushValidationEnabled());
+    assertTrue(ValidationManager.getInstance().checkPushValid());
+    assertTrue(dialogPresentedFlags[0]);
+    assertTrue(dialogPresentedFlags[1]);
+  }
+  
 
   /**
    * <p><b>Description:</b> @OxygenAPIWrapper usefully only when the getMainFiles is not accessible.</p>
@@ -184,10 +318,10 @@ public class PrePushValidationTest extends GitTestBase {
    */ 
   @Test
   public void testPrePushValidationWhenPushIsRejected() throws Exception {
-    gitAccess.setRepositorySynchronously(FIRST_LOCAL_TEST_REPOSITPRY);
-
     OptionsManager.getInstance().setValidateMainFilesBeforePush(true);
     OptionsManager.getInstance().setRejectPushOnValidationProblems(true);
+    
+    commitOneFile(FIRST_LOCAL_TEST_REPOSITORY, "ttt.txt", "");
     
     final StandalonePluginWorkspace spw =  (StandalonePluginWorkspace) PluginWorkspaceProvider.getPluginWorkspace();
     final ProjectController projectController = Mockito.mock(ProjectController.class);
@@ -247,19 +381,7 @@ public class PrePushValidationTest extends GitTestBase {
         });
     
     ValidationManager.getInstance().setPrePushValidator(new PrePushValidation(
-        validator, null) {
-      @Override
-      public boolean isEnabled() {
-        boolean toReturn = super.isEnabled();
-        try {
-          toReturn |= !(GitAccess.getInstance().getPushesAhead() > 0);
-        } catch (RepoNotInitializedException e) {
-          toReturn = true;
-          e.printStackTrace();
-        }
-        return toReturn;
-      }
-    });
+        validator, null));
    
     assertTrue(PluginWorkspaceProvider.getPluginWorkspace() instanceof StandalonePluginWorkspace);
     assertTrue(ValidationManager.getInstance().isPrePushValidationEnabled());
@@ -268,103 +390,7 @@ public class PrePushValidationTest extends GitTestBase {
     assertFalse(dialogPresentedFlags[1]);
   }
   
-  /**
-   * <p><b>Description:</b> This test cover pre-push validation behavior for case when this option
-   * is enabled and the push should not be rejected on validation problem.</p>
-   * 
-   * <p><b>Bug ID:</b> EXM-50426</p>
-   *
-   * @author Alex_Smarandache
-   *
-   */ 
-  @Test
-  public void testPrePushValidationWhenPushIsNotRejected() throws Exception {
-    gitAccess.setRepositorySynchronously(FIRST_LOCAL_TEST_REPOSITPRY);
-
-    OptionsManager.getInstance().setValidateMainFilesBeforePush(true);
-    OptionsManager.getInstance().setRejectPushOnValidationProblems(false);
-    
-    final StandalonePluginWorkspace spw =  (StandalonePluginWorkspace) PluginWorkspaceProvider.getPluginWorkspace();
-    final ProjectController projectController = Mockito.mock(ProjectController.class);
-    Mockito.when(projectController.getCurrentProjectURL()).thenReturn(
-        firstLocalRepo.getDirectory().toURI().toURL());
-    Mockito.when(spw.getProjectManager()).thenReturn(projectController);
-    // Create a custom dialog to return a custom result. Usefully to simulate a dialog showing.
-    final int[] dialogResult = new int[1];
-    dialogResult[0] = OKCancelDialog.RESULT_OK;
-    final MessageDialog dialog = Mockito.mock(MessageDialog.class);
-    Mockito.when(dialog.getResult()).then((Answer<Integer>) 
-        invocation -> {
-          return dialogResult[0];
-        }); 
-
-    // Detects if the "Push anyway" button is available and the dialog is shows.
-    final boolean[] dialogPresentedFlags = new boolean[2];
-    dialogPresentedFlags[0] = false;
-    dialogPresentedFlags[1] = false;
-
-    MessagePresenterProvider.setBuilder(new MessageDialogBuilder(
-        "test_push", DialogType.ERROR) {
-
-      @Override
-      public MessageDialogBuilder setOkButtonName(String okButtonName) {
-        dialogPresentedFlags[1] = Tags.PUSH_ANYWAY.equals(okButtonName);
-        return super.setOkButtonName(okButtonName);
-      }
-
-      @Override
-      public MessageDialog buildAndShow() {
-        dialogPresentedFlags[0] = true;
-        return dialog;
-      }
-    });
-
-    // Create a custom collector constructed to behave as if it contains validation problems
-    final ICollector collector = Mockito.mock(ICollector.class);
-    Mockito.when(collector.isEmpty()).then((Answer<Boolean>) 
-        invocation -> {
-          return false;
-        });
-    Mockito.when(collector.getAll()).then((Answer<DocumentPositionedInfo[]>) 
-        invocation -> {
-          return new DocumentPositionedInfo[0];
-        }); 
-
-    // A custom validator that is always available and return the custom collector created before
-    final IValidator validator = Mockito.mock(IValidator.class);
-    Mockito.when(validator.isAvailable()).then((Answer<Boolean>) 
-        invocation -> {
-          return true;
-        });
-    Mockito.when(validator.getCollector()).then((Answer<ICollector>) 
-        invocation -> {
-          return collector;
-        });
-
-    ValidationManager.getInstance().setPrePushValidator(new PrePushValidation(
-        validator, null) {
-      @Override
-      public boolean isEnabled() {
-        boolean toReturn = super.isEnabled();
-        try {
-          toReturn |= !(GitAccess.getInstance().getPushesAhead() > 0);
-        } catch (RepoNotInitializedException e) {
-          toReturn = true;
-          e.printStackTrace();
-        }
-        return toReturn;
-      }
-    });
-    gitAccess.add(new FileStatus(GitChangeType.ADD, "test.txt"));
-    gitAccess.commit("file test added");
-   
-    assertTrue(PluginWorkspaceProvider.getPluginWorkspace() instanceof StandalonePluginWorkspace);
-    assertTrue(ValidationManager.getInstance().isPrePushValidationEnabled());
-    assertTrue(ValidationManager.getInstance().checkPushValid());
-    assertTrue(dialogPresentedFlags[0]);
-    assertTrue(dialogPresentedFlags[1]);
-  }
-
+  
   /**
    * <p><b>Description:</b> This test cover pre-push validation behavior for case when this option
    * is enabled and the push should not be rejected on validation problem and the not same project is loaded 
@@ -395,6 +421,7 @@ public class PrePushValidationTest extends GitTestBase {
           return dialogResult[0];
         }); 
 
+    commitOneFile(FIRST_LOCAL_TEST_REPOSITORY, "ttt.txt", "");
     // Detects if the "Push anyway" button is available and the dialog is shows.
     final boolean[] dialogPresentedFlags = new boolean[3];
     dialogPresentedFlags[0] = false;
@@ -446,19 +473,7 @@ public class PrePushValidationTest extends GitTestBase {
         });
 
     ValidationManager.getInstance().setPrePushValidator(new PrePushValidation(
-        validator, null) {
-      @Override
-      public boolean isEnabled() {
-        boolean toReturn = super.isEnabled();
-        try {
-          toReturn |= !(GitAccess.getInstance().getPushesAhead() > 0);
-        } catch (RepoNotInitializedException e) {
-          toReturn = true;
-          e.printStackTrace();
-        }
-        return toReturn;
-      }
-    });
+        validator, null));
     
     assertTrue(PluginWorkspaceProvider.getPluginWorkspace() instanceof StandalonePluginWorkspace);
     assertTrue(ValidationManager.getInstance().isPrePushValidationEnabled());
@@ -481,7 +496,7 @@ public class PrePushValidationTest extends GitTestBase {
   public void testPrePushValidationWhenUncommitedChangesExist() throws Exception {
     OptionsManager.getInstance().setValidateMainFilesBeforePush(true);
     OptionsManager.getInstance().setRejectPushOnValidationProblems(false);
-    
+    commitOneFile(FIRST_LOCAL_TEST_REPOSITORY, "ttt.txt", "");
     StandalonePluginWorkspace spw =  (StandalonePluginWorkspace) PluginWorkspaceProvider.getPluginWorkspace();
     final ProjectController projectController = Mockito.mock(ProjectController.class);
     Mockito.when(projectController.getCurrentProjectURL()).thenReturn(
@@ -546,19 +561,7 @@ public class PrePushValidationTest extends GitTestBase {
           return collector;
         });
     ValidationManager.getInstance().setPrePushValidator(new PrePushValidation(
-        validator, null) {
-      @Override
-      public boolean isEnabled() {
-        boolean toReturn = super.isEnabled();
-        try {
-          toReturn |= !(GitAccess.getInstance().getPushesAhead() > 0);
-        } catch (RepoNotInitializedException e) {
-          toReturn = true;
-          e.printStackTrace();
-        }
-        return toReturn;
-      }
-    });
+        validator, null));
     assertTrue(PluginWorkspaceProvider.getPluginWorkspace() instanceof StandalonePluginWorkspace);
     assertTrue(gitAccess.hasFilesChanged());
     assertTrue(ValidationManager.getInstance().isPrePushValidationEnabled());
@@ -674,33 +677,6 @@ public class PrePushValidationTest extends GitTestBase {
     assertFalse(dialogPresentedFlags[0]);
     assertFalse(dialogPresentedFlags[1]);
     assertFalse(dialogPresentedFlags[2]);
-  }
-  
-  @Override
-  public void tearDown() throws Exception {
-    super.tearDown();
-
-    gitAccess.closeRepo();
-    flushAWT();
-    firstLocalRepo.close();
-    flushAWT();
-    remoteRepo.close();
-    flushAWT();
-    secondLocalRepo.close();
-    flushAWT();
-    try {
-      File dirToDelete = new File(FIRST_LOCAL_TEST_REPOSITPRY);
-      FileUtils.deleteDirectory(dirToDelete);
-      flushAWT();
-      dirToDelete = new File(REMOTE_TEST_REPOSITPRY);
-      FileUtils.deleteDirectory(dirToDelete);
-      flushAWT();
-      dirToDelete = new File(SECOND_LOCAL_TEST_REPOSITPRY);
-      FileUtils.deleteDirectory(dirToDelete);
-      flushAWT();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
   }
 
 }
