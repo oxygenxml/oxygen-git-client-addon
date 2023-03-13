cd70075013a21be264c962ecf91b15e3c653cb4a
Sorin Carbunaru <sorin_carbunaru@sync.ro>
2023-03-13 09:49:26 +0200

    trying to debug problematic automated test

--- src/main/java/com/oxygenxml/git/auth/login/LoginMediator.java
+++ src/main/java/com/oxygenxml/git/auth/login/LoginMediator.java
@@ -67,10 +67,14 @@ public class LoginMediator {
     System.out.println("Intra in request");
     if(!lastDialogWasCanceled) {
       System.out.println("Inainte de afisare dialog");
-      final LoginDialog loginDialog = new LoginDialog(host, loginMessage);
-      System.out.println(loginDialog);
-      lastDialogWasCanceled = loginDialog.getResult() == OKCancelDialog.RESULT_CANCEL;
-      toReturn = Optional.of(new LoginStatusInfo(loginDialog.getCredentials(), lastDialogWasCanceled));
+      try {
+        final LoginDialog loginDialog = new LoginDialog(host, loginMessage);
+        System.out.println(loginDialog);
+        lastDialogWasCanceled = loginDialog.getResult() == OKCancelDialog.RESULT_CANCEL;
+        toReturn = Optional.of(new LoginStatusInfo(loginDialog.getCredentials(), lastDialogWasCanceled));
+      } catch (Throwable t) {
+        t.printStackTrace();
+      }
     }
     
     return toReturn;
