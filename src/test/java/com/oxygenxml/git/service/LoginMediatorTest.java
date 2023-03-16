09f4f7eabd514a3af79dff16aa8b95016b09a291
Alexandru Smarandache <alex.smarandache@yahoo.com>
2023-03-16 10:17:45 +0200

    EXM-52468: CR, refactor test with mockito

--- src/test/java/com/oxygenxml/git/service/LoginMediatorTest.java
+++ src/test/java/com/oxygenxml/git/service/LoginMediatorTest.java
@@ -1,15 +1,19 @@
 package com.oxygenxml.git.service;
 
-import javax.swing.JButton;
 import javax.swing.SwingUtilities;
 
 import org.awaitility.Awaitility;
 import org.awaitility.Duration;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.oxygenxml.git.auth.login.LoginMediator;
 import com.oxygenxml.git.auth.login.LoginStatusInfo;
-import com.oxygenxml.git.translator.Tags;
+import com.oxygenxml.git.view.dialog.LoginDialog;
 
 import ro.sync.exml.workspace.api.standalone.ui.OKCancelDialog;
 
@@ -18,7 +22,9 @@ import ro.sync.exml.workspace.api.standalone.ui.OKCancelDialog;
  * 
  * @author alex_smarandache
  */
-public class LoginMediatorTest extends GitTestBase {
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(LoginMediator.class)
+public class LoginMediatorTest {
 
   /**
    * <p><b>Description:</b> The login dialog should appear only once if it was closed by the user.</p>
@@ -26,27 +32,36 @@ public class LoginMediatorTest extends GitTestBase {
    * <p><b>Bug ID:</b> EXM-52468</p>
    *
    * @author Alex_Smarandache
+   * 
+   * @throws Exception 
    */ 
   @Test
-  public void testTheLoginDialogApparences() {
-    LoginMediator.getInstance().reset();
-    final LoginStatusInfo[] loginInfo = new LoginStatusInfo[1];
-    SwingUtilities.invokeLater(() -> {
-      loginInfo[0] = LoginMediator.getInstance().requestLogin("host", "loginMessage").orElse(null);
-    });
+  public void testTheLoginDialogApparences() throws Exception {
+    try {
+      final String host = "github.com";
+      final String loginMessage = "Please login";
+      final LoginDialog mockLoginDialog = Mockito.mock(LoginDialog.class);
+      Mockito.when(mockLoginDialog.getResult()).thenReturn(OKCancelDialog.RESULT_CANCEL);
+      PowerMockito.whenNew(LoginDialog.class).withArguments(host, loginMessage).thenReturn(mockLoginDialog);
+      LoginMediator.getInstance().reset();
+      
+      final LoginStatusInfo[] loginInfo = new LoginStatusInfo[1];
+      SwingUtilities.invokeLater(() -> {
+        loginInfo[0] = LoginMediator.getInstance().requestLogin(host, loginMessage).orElse(null);
+      });
 
-    final OKCancelDialog loginDialog = (OKCancelDialog) findDialog(Tags.LOGIN_DIALOG_TITLE);
-    assertNotNull(loginDialog);
-    final JButton cancelButton = findFirstButton(loginDialog, Tags.CANCEL);
-    assertNotNull(cancelButton);
-    cancelButton.doClick();
-    Awaitility.await().atMost(Duration.ONE_SECOND).until(() -> loginInfo[0] != null
-        && loginInfo[0].isCanceled() && loginInfo[0].getCredentials() == null);
-    SwingUtilities.invokeLater(() -> loginInfo[0] = 
-        LoginMediator.getInstance().requestLogin("host", "loginMessage").orElse(null));
-    assertNull(findDialog(Tags.LOGIN_DIALOG_TITLE));
-    Awaitility.await().atMost(Duration.ONE_SECOND).until(() -> loginInfo[0] == null);
-    LoginMediator.getInstance().reset();
+      Awaitility.await().atMost(Duration.ONE_SECOND).until(() -> loginInfo[0] != null
+          && loginInfo[0].isCanceled() && loginInfo[0].getCredentials() == null);
+      
+      SwingUtilities.invokeLater(() -> {
+        loginInfo[0] = LoginMediator.getInstance().requestLogin(host, loginMessage).orElse(null);
+      });
+     
+      Awaitility.await().atMost(Duration.ONE_SECOND).until(() -> loginInfo[0] == null);
+    } finally {
+      LoginMediator.getInstance().reset();
+    } 
+    
   }
-
+  
 }
