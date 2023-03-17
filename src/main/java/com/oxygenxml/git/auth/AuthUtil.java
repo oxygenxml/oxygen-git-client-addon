3207f9dd2719b3387463cd20a03721e5bf5b9ad3
Alexandru Smarandache <alex.smarandache@yahoo.com>
2023-03-17 14:09:27 +0200

    EXM-52774: refactor code to reduce cognitive complexity

--- src/main/java/com/oxygenxml/git/auth/AuthUtil.java
+++ src/main/java/com/oxygenxml/git/auth/AuthUtil.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.apache.sshd.common.SshConstants;
 import org.apache.sshd.common.SshException;
+import org.eclipse.jgit.annotations.NonNull;
 import org.eclipse.jgit.annotations.Nullable;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.RefNotAdvertisedException;
@@ -109,10 +110,10 @@ public class AuthUtil {
    * @return <code>true</code> if the authentication should be tried again.
    */
   public static boolean handleAuthException(
-      GitAPIException ex,
-      String hostName,
-      AuthExceptionMessagePresenter excMessPresenter,
-      boolean retryLoginHere) {
+      final @NonNull GitAPIException ex,
+      final @NonNull String hostName,
+      final @Nullable AuthExceptionMessagePresenter excMessPresenter,
+      final boolean retryLoginHere) {
     
     if (LOGGER.isDebugEnabled()) {
       LOGGER.debug("Handle Auth Exception: ");
@@ -130,26 +131,9 @@ public class AuthUtil {
     String lowercaseMsg = ex.getMessage().toLowerCase();
     if (lowercaseMsg.contains(NOT_AUTHORIZED) 
         || lowercaseMsg.contains(AUTHENTICATION_NOT_SUPPORTED)) {
-      // Authorization problems.
-      String loginMessage = TRANSLATOR.getTranslation(Tags.AUTHENTICATION_FAILED) + " ";
-      if (userCredentials.getType() == CredentialsType.USER_AND_PASSWORD) {
-        String username = ((UserAndPasswordCredentials) userCredentials).getUsername();
-        loginMessage += username == null ? TRANSLATOR.getTranslation(Tags.NO_CREDENTIALS_FOUND)
-            : TRANSLATOR.getTranslation(Tags.CHECK_CREDENTIALS);
-      } else if (userCredentials.getType() == CredentialsType.PERSONAL_ACCESS_TOKEN) {
-        loginMessage += TRANSLATOR.getTranslation(Tags.CHECK_TOKEN_VALUE_AND_PERMISSIONS);
-      }
-      tryAgainOutside = shouldTryAgainOutside(hostName, retryLoginHere, loginMessage);
+      tryAgainOutside = treatAuthorizationProblems(hostName, retryLoginHere, userCredentials);
     } else if (lowercaseMsg.contains(NOT_PERMITTED)) {
-      // The user doesn't have permissions.
-      PluginWorkspaceProvider.getPluginWorkspace()
-          .showErrorMessage(TRANSLATOR.getTranslation(Tags.NO_RIGHTS_TO_PUSH_MESSAGE));
-      String loginMessage = TRANSLATOR.getTranslation(Tags.LOGIN_DIALOG_CREDENTIALS_DOESNT_HAVE_RIGHTS); 
-      if (userCredentials.getType() == CredentialsType.USER_AND_PASSWORD) {
-        loginMessage += ", " + ((UserAndPasswordCredentials) userCredentials).getUsername();
-      }
-      loginMessage += ".";
-      tryAgainOutside = shouldTryAgainOutside(hostName, retryLoginHere, loginMessage);
+      tryAgainOutside = treatUserAccessDenied(hostName, retryLoginHere, userCredentials);
     } else if (lowercaseMsg.contains(ORIGIN_NOT_FOUND)
         || lowercaseMsg.contains(NO_VALUE_FOR_ORIGIN_URL_IN_CONFIG)) {
       // No remote linked with the local.
@@ -163,29 +147,106 @@ public class AuthUtil {
       tryAgainOutside = passphrase != null;
     } else if (ex.getCause() instanceof NoRemoteRepositoryException
         || lowercaseMsg.contains("invalid advertisement of")) {
-      if (excMessPresenter != null) {
-        if (userCredentials.getType() == CredentialsType.USER_AND_PASSWORD) {
-          excMessPresenter.presentMessage(TRANSLATOR.getTranslation(Tags.CLONE_REPOSITORY_DIALOG_URL_IS_NOT_A_REPOSITORY));
-        } else if (userCredentials.getType() == CredentialsType.PERSONAL_ACCESS_TOKEN) {
-          excMessPresenter.presentMessage(TRANSLATOR.getTranslation(Tags.CANNOT_REACH_HOST) + ". "
-              + TRANSLATOR.getTranslation(Tags.CHECK_TOKEN_VALUE_AND_PERMISSIONS));
-        }
-      } else {
-        LOGGER.error(ex.getMessage(), ex);
-      }
+      treatAuthenticationHostProblem(ex, excMessPresenter, userCredentials);
     } else if (ex instanceof TransportException) {
       treatTransportException((TransportException)ex, excMessPresenter);
     } else {
       // "Unhandled" exception
-      if (excMessPresenter != null) {
-        excMessPresenter.presentMessage(ex instanceof RefNotAdvertisedException ? 
-            TRANSLATOR.getTranslation(Tags.NO_REMOTE_EXCEPTION_MESSAGE) : 
-          ex.getClass().getName() + ": " + ex.getMessage());
-      } else {
-        LOGGER.error(ex.getMessage(), ex);
-      }
+      treatUnhandledException(ex, excMessPresenter);
     }
     
+    return tryAgainOutside;
+  }
+
+  /**
+   * This method treats an authentication problem, for example user is logged but the repository which is wanted to be cloned is not found.
+   *  
+   * @param ex                  The original Git exception.
+   * @param excMessPresenter    The presenter message presenter.
+   * @param userCredentials     The user credentials.
+   */
+  private static void treatAuthenticationHostProblem(
+      @NonNull final GitAPIException ex, 
+      @Nullable final AuthExceptionMessagePresenter excMessPresenter,
+      @NonNull final CredentialsBase userCredentials) {
+    if (excMessPresenter != null) {
+      if (userCredentials.getType() == CredentialsType.USER_AND_PASSWORD) {
+        excMessPresenter.presentMessage(TRANSLATOR.getTranslation(Tags.CLONE_REPOSITORY_DIALOG_URL_IS_NOT_A_REPOSITORY));
+      } else if (userCredentials.getType() == CredentialsType.PERSONAL_ACCESS_TOKEN) {
+        excMessPresenter.presentMessage(TRANSLATOR.getTranslation(Tags.CANNOT_REACH_HOST) + ". "
+            + TRANSLATOR.getTranslation(Tags.CHECK_TOKEN_VALUE_AND_PERMISSIONS));
+      }
+    } else {
+      LOGGER.error(ex.getMessage(), ex);
+    }
+  }
+
+  /**
+   * This method is used to treat an unhandlead exception.
+   * 
+   * @param ex                  The original Git exception.
+   * @param excMessPresenter    The presenter message presenter.
+   */
+  private static void treatUnhandledException(
+      @NonNull final GitAPIException ex, 
+      @Nullable final AuthExceptionMessagePresenter excMessPresenter) {
+    if (excMessPresenter != null) {
+      excMessPresenter.presentMessage(ex instanceof RefNotAdvertisedException ? 
+          TRANSLATOR.getTranslation(Tags.NO_REMOTE_EXCEPTION_MESSAGE) : 
+        ex.getClass().getName() + ": " + ex.getMessage());
+    } else {
+      LOGGER.error(ex.getMessage(), ex);
+    }
+  }
+
+  /**
+   * This method is used the exception that appears when the user doesn't have permission to do an action.
+   * 
+   * @param hostName        The name of the host.
+   * @param retryLoginHere  <code>true</code> to retry login here.
+   * @param userCredentials The user credentials.
+   * 
+   * @return <code>true</code> to try again to login outside.
+   */
+  private static boolean treatUserAccessDenied(
+      final @NonNull String hostName, 
+      final boolean retryLoginHere,
+      final @NonNull CredentialsBase userCredentials) {
+    boolean tryAgainOutside = false;
+    PluginWorkspaceProvider.getPluginWorkspace()
+        .showErrorMessage(TRANSLATOR.getTranslation(Tags.NO_RIGHTS_TO_PUSH_MESSAGE));
+    String loginMessage = TRANSLATOR.getTranslation(Tags.LOGIN_DIALOG_CREDENTIALS_DOESNT_HAVE_RIGHTS); 
+    if (userCredentials.getType() == CredentialsType.USER_AND_PASSWORD) {
+      loginMessage += ", " + ((UserAndPasswordCredentials) userCredentials).getUsername();
+    }
+    loginMessage += ".";
+    tryAgainOutside = shouldTryAgainOutside(hostName, retryLoginHere, loginMessage);
+    return tryAgainOutside;
+  }
+
+  /**
+   * This method is used to treat authorization problems.
+   * 
+   * @param hostName        The name of the host.
+   * @param retryLoginHere  <code>true</code> to retry login here.
+   * @param userCredentials The user credentials.
+   * 
+   * @return <code>true</code> to try again to login outside.
+   */
+  private static boolean treatAuthorizationProblems(
+      final @NonNull String hostName, 
+      final boolean retryLoginHere,
+      final @NonNull CredentialsBase userCredentials) {
+    boolean tryAgainOutside = false;
+    String loginMessage = TRANSLATOR.getTranslation(Tags.AUTHENTICATION_FAILED) + " ";
+    if (userCredentials.getType() == CredentialsType.USER_AND_PASSWORD) {
+      final String username = ((UserAndPasswordCredentials) userCredentials).getUsername();
+      loginMessage += username == null ? TRANSLATOR.getTranslation(Tags.NO_CREDENTIALS_FOUND)
+          : TRANSLATOR.getTranslation(Tags.CHECK_CREDENTIALS);
+    } else if (userCredentials.getType() == CredentialsType.PERSONAL_ACCESS_TOKEN) {
+      loginMessage += TRANSLATOR.getTranslation(Tags.CHECK_TOKEN_VALUE_AND_PERMISSIONS);
+    }
+    tryAgainOutside = shouldTryAgainOutside(hostName, retryLoginHere, loginMessage);
     return tryAgainOutside;
   }
 
