e958772183a69d9be656fa060bdd7c95bf180a66
Alexandru Smarandache <alex.smarandache@yahoo.com>
2023-07-21 16:10:37 +0300

    EXM-43578: use system lfs, not jgit builtin lfs

--- src/main/java/com/oxygenxml/git/service/lfs/LFSSupport.java
+++ src/main/java/com/oxygenxml/git/service/lfs/LFSSupport.java
@@ -1,15 +1,8 @@
 package com.oxygenxml.git.service.lfs;
 
 import org.eclipse.jgit.lfs.BuiltinLFS;
-import org.eclipse.jgit.util.LfsFactory;
-import org.eclipse.jgit.util.LfsFactory.LfsInstallCommand;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.oxygenxml.git.service.GitEventAdapter;
 import com.oxygenxml.git.view.event.GitController;
-import com.oxygenxml.git.view.event.GitEventInfo;
-import com.oxygenxml.git.view.event.GitOperation;
 
 /**
  * Class related to the large-files-system in Git.
@@ -17,11 +10,6 @@ import com.oxygenxml.git.view.event.GitOperation;
  * @author alex_smarandache
  */
 public class LFSSupport {
-  
-  /**
-   * Logger for logging.
-   */
-  private static final Logger LOGGER = LoggerFactory.getLogger(LFSSupport.class);
 
   /**
    * Hidden constructor.
@@ -37,21 +25,6 @@ public class LFSSupport {
    */
   public static void install(final GitController gitCtrl) {
   	BuiltinLFS.register();
-    gitCtrl.addGitListener(new GitEventAdapter() {
-      @Override
-      public void operationSuccessfullyEnded(GitEventInfo info) {
-        if(info.getGitOperation() == GitOperation.OPEN_WORKING_COPY) {
-          try {
-            LfsInstallCommand installCommand = LfsFactory.getInstance().getInstallCommand();
-            if (installCommand != null) {
-              installCommand.setRepository(gitCtrl.getGitAccess().getRepository()).call();
-            }
-          } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-          }
-        }
-      }
-    });
   }
   
 }
