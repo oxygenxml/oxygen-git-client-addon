3a9fd0d3e35574b2a2ee1ba5d6cc007aaf235e90
Alexandru Smarandache <alex_smarandache@sync.ro>
2023-12-13 14:59:19 +0200

    EXM-47707: refactor content reloader using the new API

--- src/main/java/com/oxygenxml/git/EditorContentReloader.java
+++ src/main/java/com/oxygenxml/git/EditorContentReloader.java
@@ -1,19 +1,12 @@
 package com.oxygenxml.git;
 
-import java.util.Collections;
-import java.util.Map;
-
-import javax.swing.AbstractAction;
+import java.lang.reflect.Method;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ro.sync.exml.workspace.api.PluginWorkspace;
 import ro.sync.exml.workspace.api.editor.WSEditor;
-import ro.sync.exml.workspace.api.editor.page.WSEditorPage;
-import ro.sync.exml.workspace.api.editor.page.author.WSAuthorEditorPage;
-import ro.sync.exml.workspace.api.editor.page.ditamap.WSDITAMapEditorPage;
-import ro.sync.exml.workspace.api.editor.page.text.WSTextEditorPage;
 import ro.sync.exml.workspace.api.standalone.StandalonePluginWorkspace;
 
 /**
@@ -22,18 +15,13 @@ import ro.sync.exml.workspace.api.standalone.StandalonePluginWorkspace;
  * @author alex_smarandache
  */
 public class EditorContentReloader {
-  
-  /**
-   * The reload action identifier.
-   */
-  public static final String RELOAD_ACTION_ID = "File/Reload";
-  
+
   /**
    * Logger for logging.
    */
   private static final Logger LOGGER = LoggerFactory.getLogger(EditorContentReloader.class);
-  
-  
+
+
   /**
    * Private constructor.
    * 
@@ -42,53 +30,35 @@ public class EditorContentReloader {
   private EditorContentReloader() {
     throw new UnsupportedOperationException("Instantiation of this class is not allowed!");
   }
-  
+
   /**
-   * Reload the current editor content and DMM editor.
+   * This method will reload contents(if any change exists) from the editors opened in DMM or main area.
    * 
    * @param pluginWS The plugin workspace.
    */
   public static void reloadCurrentEditor(final StandalonePluginWorkspace pluginWS) {
-    reloadEditorContent(pluginWS.getCurrentEditorAccess(PluginWorkspace.MAIN_EDITING_AREA));
-    reloadEditorContent(pluginWS.getCurrentEditorAccess(PluginWorkspace.DITA_MAPS_EDITING_AREA));
+    reloadEditor(pluginWS, PluginWorkspace.MAIN_EDITING_AREA);
+    reloadEditor(pluginWS, PluginWorkspace.DITA_MAPS_EDITING_AREA);
   }
 
   /**
-   * Reload the content for the given editor.
+   * This method reload the current editor with the given identifier.
    * 
-   * @param editor The current editor to reload its content.
+   * @param pluginWS          The plugin workspace.
+   * @param editorIdentifier  The identifier of the editor.
    */
-  private static void reloadEditorContent(final WSEditor editor) {
-    if(editor == null) {
-      return;
+  private static void reloadEditor(final StandalonePluginWorkspace pluginWS, final int editorIdentifier) {
+    // TODO update it after the 26.1 Oxygen API will be the minimum version required
+    final WSEditor editor = pluginWS.getCurrentEditorAccess(editorIdentifier);
+    if (editor != null) {
+      try {
+        final Class<?> editorClass = editor.getClass();
+        final Method reloadMethod = editorClass.getMethod("reloadIfChangeOnDiskDetected");
+        reloadMethod.invoke(editor);
+      } catch (Exception e) {
+        LOGGER.debug(e.getMessage(), e);
+      }
     }
-    
-    AbstractAction reloadAction = null;
-    final WSEditorPage page = editor.getCurrentPage();
-    Map<String, Object> actions = Collections.emptyMap();
-    
-    if(page instanceof WSDITAMapEditorPage) {
-      final WSDITAMapEditorPage mapPage = (WSDITAMapEditorPage)page;
-      actions = mapPage.getActionsProvider().getActions();
-    } else if(page instanceof WSAuthorEditorPage) {
-      final WSAuthorEditorPage authorPage = (WSAuthorEditorPage)page;
-      actions = authorPage.getActionsProvider().getAuthorCommonActions();
-    } else if(page instanceof WSTextEditorPage) {
-      final WSTextEditorPage authorPage = (WSTextEditorPage)page;
-      actions = authorPage.getActionsProvider().getTextActions();
-    } 
-    
-    final Object actionObj = actions.get(RELOAD_ACTION_ID);
-    if(actionObj instanceof AbstractAction) {
-      reloadAction = (AbstractAction) actionObj;
-      if(reloadAction != null) {
-        reloadAction.actionPerformed(null);
-      } 
-    } else {
-      final Exception ex = new Exception("The reload action not found.");
-      LOGGER.error(ex.getMessage(), ex);
-    }
-    
   }
 
 }
--- src/test/java/com/oxygenxml/git/utils/EditorContentReloaderTest.java
+++ src/test/java/com/oxygenxml/git/utils/EditorContentReloaderTest.java
@@ -1,10 +1,10 @@
 package com.oxygenxml.git.utils;
 
-import java.awt.event.ActionEvent;
-import java.util.HashMap;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.net.URL;
 import java.util.Map;
-
-import javax.swing.AbstractAction;
 
 import org.mockito.Mockito;
 
@@ -13,10 +13,15 @@ import com.oxygenxml.git.EditorContentReloader;
 import junit.extensions.jfcunit.JFCTestCase;
 import ro.sync.exml.workspace.api.PluginWorkspace;
 import ro.sync.exml.workspace.api.editor.WSEditor;
-import ro.sync.exml.workspace.api.editor.page.author.WSAuthorEditorPage;
-import ro.sync.exml.workspace.api.editor.page.author.actions.AuthorActionsProvider;
-import ro.sync.exml.workspace.api.editor.page.text.WSTextEditorPage;
-import ro.sync.exml.workspace.api.editor.page.text.actions.TextActionsProvider;
+import ro.sync.exml.workspace.api.editor.documenttype.DocumentTypeInformation;
+import ro.sync.exml.workspace.api.editor.page.WSEditorPage;
+import ro.sync.exml.workspace.api.editor.transformation.TransformationFeedback;
+import ro.sync.exml.workspace.api.editor.transformation.TransformationScenarioNotFoundException;
+import ro.sync.exml.workspace.api.editor.validation.OperationInProgressException;
+import ro.sync.exml.workspace.api.editor.validation.ValidationProblemsFilter;
+import ro.sync.exml.workspace.api.editor.validation.ValidationScenarioNotFoundException;
+import ro.sync.exml.workspace.api.listeners.WSEditorListener;
+import ro.sync.exml.workspace.api.listeners.WSEditorPageChangedListener;
 import ro.sync.exml.workspace.api.standalone.StandalonePluginWorkspace;
 
 /**
@@ -35,64 +40,217 @@ public class EditorContentReloaderTest extends JFCTestCase {
    *
    */
   public void testReloadCurrentEditor() {
-    final int[] textPageReloadCalls = { 0 };
-    final int[] authorPageReloadCalls = { 0 };
-    final WSEditor editor = Mockito.mock(WSEditor.class);
-    
-    final WSAuthorEditorPage authorPage = Mockito.mock(WSAuthorEditorPage.class);
-    Mockito.when(editor.getCurrentPage()).thenReturn(authorPage);
-    final AuthorActionsProvider authorActionsProvider = Mockito.mock(AuthorActionsProvider.class);
-    final AbstractAction authorReloadAct = new AbstractAction() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        authorPageReloadCalls[0]++;
-      }
-    };
-    final Map<String, Object> authorCommonActions = new HashMap<>();
-    authorCommonActions.put(EditorContentReloader.RELOAD_ACTION_ID, authorReloadAct);
-    Mockito.when(authorActionsProvider.getAuthorCommonActions()).thenReturn(authorCommonActions);
-    Mockito.when(authorPage.getActionsProvider()).thenReturn(authorActionsProvider);
-    
     final StandalonePluginWorkspace spw = Mockito.mock(StandalonePluginWorkspace.class);
-    Mockito.when(spw.getCurrentEditorAccess(PluginWorkspace.MAIN_EDITING_AREA)).thenReturn(editor);
-    
-    assertEquals(0, textPageReloadCalls[0]);
-    assertEquals(0, authorPageReloadCalls[0]);
    
-    EditorContentReloader.reloadCurrentEditor(spw);
-    assertEquals(0, textPageReloadCalls[0]);
-    assertEquals(1, authorPageReloadCalls[0]);
+    final int[] mainAreaReloadCalls = { 0 };
+    final WSEditorAdapter mainAreaEditor = new WSEditorAdapter();
+    mainAreaEditor.setReloadIfChangeOnDiskDetectedAction(() -> mainAreaReloadCalls[0]++);
+    Mockito.when(spw.getCurrentEditorAccess(PluginWorkspace.MAIN_EDITING_AREA)).thenReturn(mainAreaEditor);
+    
+    final int[] dmmReloadCalls = { 0 };
+    final WSEditorAdapter dmmEditor = new WSEditorAdapter();
+    Mockito.when(spw.getCurrentEditorAccess(PluginWorkspace.DITA_MAPS_EDITING_AREA)).thenReturn(dmmEditor);
+    
+    assertEquals(0, mainAreaReloadCalls[0]);
+    assertEquals(0, dmmReloadCalls[0]);
     
     EditorContentReloader.reloadCurrentEditor(spw);
-    assertEquals(0, textPageReloadCalls[0]);
-    assertEquals(2, authorPageReloadCalls[0]);
+    assertEquals(1, mainAreaReloadCalls[0]);
+    assertEquals(0, dmmReloadCalls[0]);
     
-    final WSTextEditorPage textPage = Mockito.mock(WSTextEditorPage.class);
-    final TextActionsProvider textActionsProvider = Mockito.mock(TextActionsProvider.class);
-    final AbstractAction textReloadAct = new AbstractAction() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        textPageReloadCalls[0]++;
+    dmmEditor.setReloadIfChangeOnDiskDetectedAction(() -> dmmReloadCalls[0]++);
+    
+    EditorContentReloader.reloadCurrentEditor(spw);
+    assertEquals(2, mainAreaReloadCalls[0]);
+    assertEquals(1, dmmReloadCalls[0]);
+    
+    mainAreaEditor.setReloadIfChangeOnDiskDetectedAction(null);
+    
+    EditorContentReloader.reloadCurrentEditor(spw);
+    assertEquals(2, mainAreaReloadCalls[0]);
+    assertEquals(2, dmmReloadCalls[0]);
+  }
+  
+  
+  /**
+   * An adaptor for a WS Editor.
+   * 
+   * @author alex_smarandache
+   */
+  public class WSEditorAdapter implements WSEditor {
+    
+    private Runnable reloadIfChangeOnDiskDetectedAction;
+
+    @Override
+    public String getEncodingForSerialization() {
+      return null;
+    }
+
+    @Override
+    public URL getEditorLocation() {
+      return null;
+    }
+
+    @Override
+    public void save() {
+    }
+
+    @Override
+    public void saveAs(URL location) { 
+    }
+
+    @Override
+    public boolean close(boolean askForSave) {
+      return false;
+    }
+
+    @Override
+    public void setModified(boolean modified) {
+    }
+
+    @Override
+    public boolean isNewDocument() {
+      return false;
+    }
+
+    @Override
+    public Reader createContentReader() {
+      return null;
+    }
+
+    @Override
+    public InputStream createContentInputStream() throws IOException {
+      return null;
+    }
+
+    @Override
+    public void reloadContent(Reader reader) {
+    }
+
+    @Override
+    public void reloadContent(Reader reader, boolean discardUndoableEdits) {
+    }
+
+    @Override
+    public void setEditorTabText(String tabText) {
+    }
+
+    @Override
+    public void setEditorTabTooltipText(String tabTooltip) { 
+    }
+
+    @Override
+    public DocumentTypeInformation getDocumentTypeInformation() {
+      return null;
+    }
+
+    @Override
+    public boolean isModified() {
+      return false;
+    }
+
+    @Override
+    public void runTransformationScenarios(String[] scenarioNames, TransformationFeedback transformationFeedback)
+        throws TransformationScenarioNotFoundException {  
+    }
+
+    @Override
+    public void runTransformationScenario(String scenarioName, Map<String, String> scenarioParameters,
+        TransformationFeedback transformationFeedback) throws TransformationScenarioNotFoundException {
+    }
+
+    @Override
+    public void stopCurrentTransformationScenario() { 
+    }
+
+    @Override
+    public Thread runValidationScenarios(String[] scenarioNames)
+        throws ValidationScenarioNotFoundException, OperationInProgressException {
+      return null;
+    }
+
+    @Override
+    public WSEditorPage getCurrentPage() {
+      return null;
+    }
+
+    @Override
+    public String getCurrentPageID() {
+      return null;
+    }
+
+    @Override
+    public void addPageChangedListener(WSEditorPageChangedListener pageChangedListener) {
+    }
+
+    @Override
+    public void removePageChangedListener(WSEditorPageChangedListener pageChangedListener) {
+    }
+
+    @Override
+    public void addEditorListener(WSEditorListener editorListener) {
+    }
+
+    @Override
+    public WSEditorListener[] getEditorListeners() {
+      return null;
+    }
+
+    @Override
+    public void removeEditorListener(WSEditorListener editorListener) {
+    }
+
+    @Override
+    public void changePage(String pageID) {
+    }
+
+    @Override
+    public void addValidationProblemsFilter(ValidationProblemsFilter validationProblemsFilter) {
+    }
+
+    @Override
+    public void removeValidationProblemsFilter(ValidationProblemsFilter validationProblemsFilter) {
+    }
+
+    @Override
+    public boolean checkValid() {
+      return false;
+    }
+
+    @Override
+    public boolean checkValid(boolean automatic) {
+      return false;
+    }
+
+    @Override
+    public Object getComponent() {
+      return null;
+    }
+
+    @Override
+    public void setEditable(boolean editable) { 
+    }
+
+    @Override
+    public boolean isEditable() {
+      return false;
+    }
+
+    @Override
+    public String getContentType() {
+      return null;
+    }
+
+    public void reloadIfChangeOnDiskDetected() {
+      if(reloadIfChangeOnDiskDetectedAction != null) {
+        reloadIfChangeOnDiskDetectedAction.run();
       }
-    };
-    final Map<String, Object> textCommonActions = new HashMap<>();
-    textCommonActions.put(EditorContentReloader.RELOAD_ACTION_ID, textReloadAct);
-    Mockito.when(textActionsProvider.getTextActions()).thenReturn(textCommonActions);
-    Mockito.when(textPage.getActionsProvider()).thenReturn(textActionsProvider);
-    Mockito.when(editor.getCurrentPage()).thenReturn(textPage);
+    }
     
-    EditorContentReloader.reloadCurrentEditor(spw);
-    assertEquals(1, textPageReloadCalls[0]);
-    assertEquals(2, authorPageReloadCalls[0]);
+    public void setReloadIfChangeOnDiskDetectedAction(Runnable reloadIfChangeOnDiskDetectedAction) {
+      this.reloadIfChangeOnDiskDetectedAction = reloadIfChangeOnDiskDetectedAction;
+    }
     
-    EditorContentReloader.reloadCurrentEditor(spw);
-    assertEquals(2, textPageReloadCalls[0]);
-    assertEquals(2, authorPageReloadCalls[0]);
-    
-    Mockito.when(editor.getCurrentPage()).thenReturn(null);
-    EditorContentReloader.reloadCurrentEditor(spw);
-    assertEquals(2, textPageReloadCalls[0]);
-    assertEquals(2, authorPageReloadCalls[0]);
   }
 
 }
