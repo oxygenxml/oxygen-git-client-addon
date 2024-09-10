fe01c1fbd558682e5174c2473f3b31addf22b7c7
Sorin Carbunaru <sorincarbunaru@users.noreply.github.com>
2024-09-10 13:16:33 +0300

    EXM-53634: the method was moved to BranchesUtil

--- src/main/java/com/oxygenxml/git/service/GitAccess.java
+++ src/main/java/com/oxygenxml/git/service/GitAccess.java
@@ -3296,37 +3296,6 @@ public class GitAccess {
 	}
 	
 	/**
-	 * Get obsolete local branches, i.e. the local branches who track deleted remote branches.
-	 * 
-	 * @return the obsolete local branches.
-	 * 
-	 * @throws NoRepositorySelected
-	 */
-	public List<Ref> getLocalBranchesThatNoLongerHaveRemotes() throws NoRepositorySelected {
-	  List<Ref> obsoleteBranches = new ArrayList<>();
-	  
-	  Repository repository = getRepository();
-	  List<Ref> localBranchList = getLocalBranchList();
-	  for (Ref localBranch : localBranchList) {
-	    final String branchName = Repository.shortenRefName(localBranch.getName());
-	    BranchConfig branchConfig = new BranchConfig(repository.getConfig(), branchName);
-	    String remoteBranch = branchConfig.getRemoteTrackingBranch();
-	    if (remoteBranch != null && !remoteBranch.isBlank()) {
-	      try {
-	        if (repository.findRef(remoteBranch) == null) {
-	          obsoleteBranches.add(localBranch);
-	        }
-	      } catch (IOException ex) {
-	        LOGGER.error(ex, ex);
-	      }
-	    }
-	  } 
-	  
-	  return obsoleteBranches;
-	}
-	
-	
-	/**
 	 * Compute a list with all remote branches. 
 	 * <br>
 	 * This means that a list will be built with each remote branch that currently exists in each remote 
